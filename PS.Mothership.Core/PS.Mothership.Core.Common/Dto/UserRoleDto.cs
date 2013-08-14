3ef9542ca5beb08b18e682816828f7993e497487
Raj.Manikand <Raj.Manikand@mycompany.com>
2013-08-14 09:20:37 +0000

    made permission enum nullable
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@9 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/UserRoleDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/UserRoleDto.cs
@@ -31,7 +31,7 @@ namespace PS.Mothership.Core.Common.Dto
     public class UserPermissionDto
     {
         [DataMember]
-        public PermissionLutEnum PermissionEnums { get; set; }
+        public PermissionLutEnum? PermissionEnums { get; set; }
 
         [DataMember]
         public long PermissionId { get; set; }
