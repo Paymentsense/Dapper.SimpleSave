6784011c3862acd7ddea3d822b1f2ae304e63a53
sai.krishna <sai.krishna@mycompany.com>
2013-08-23 11:08:38 +0000

    Adding notification service contract to Core project
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@24 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/INotificationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Contracts
+{
+    [ServiceContract(CallbackContract = typeof(INotificationCallback))]
+    public interface INotificationService
+    {
+        [OperationContract(IsOneWay = false)]
+        void Subscribe(Guid subscriptionId, string[] eventNames);
+        [OperationContract(IsOneWay = true)]
+        void EndSubscribe(Guid subscriptionId);
+    }
+
+
+    public interface INotificationCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void ReceiveNotification(NotificationResultDto notificationResult);
+    }
+
+    [DataContract]
+    public class NotificationResultDto
+    {
+        [DataMember]
+        public string NotificationMessage { get; set; }
+    }
+}
