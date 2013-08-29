0827c627a459da6811187cddf85921128de5c291
sai.krishna <sai.krishna@mycompany.com>
2013-08-29 13:20:10 +0000

    Changes to handle Subscribe duplex wcf service only once per application.
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@34 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Constants/GlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Constants
+{
+    public class GlobalConstants
+    {
+        public const string ApplicationName = "PS.WEB.UI";
+    }
+}
