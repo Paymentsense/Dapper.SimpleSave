4260a62bc7ffc5d87e0d809e705425ee93fea73a
Raj.Manikand <Raj.Manikand@mycompany.com>
2013-09-05 11:38:36 +0000

    new property added to AccountDto
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@45 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/AccountDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/AccountDto.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
+using System.Web.Security;
 
 namespace PS.Mothership.Core.Common.Dto
 {
     [DataContract]
     public class AccountDto
     {
+        [DataMember]
+        public long UserId { get; set; }
+
+        [DataMember]
+        public MembershipCreateStatus Status { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+
         [DataMember]
         public UserProfileDto UserProfileDto { get;  set; }
 
