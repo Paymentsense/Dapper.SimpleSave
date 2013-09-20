bfc15f974fb293bb2bf2c221ecd70753e6de92ed
Raj.Manikand <Raj.Manikand@mycompany.com>
2013-09-20 15:14:43 +0000

    Test for util helper
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@59 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.UnitTest/Helper/UtilTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PS.Mothership.Core.Common.Helper;
+
+namespace PS.Mothership.Core.UnitTest.Helper
+{
+    [TestFixture]
+    public class UtilTest
+    {
+        [Test]
+        public void ConvertToJson_AnObjectGiven_GiveBackAJasonString()
+        {
+            // Arrange 
+            var p = new Person { Id = 1, Name = "Name" };
+            const string actual = @"{""Id"":1";
+
+            // Act
+            string jsonString = Util.ConvertToJson(p);
+
+
+            //Console.WriteLine(jsonString);
+            //Console.WriteLine(actual);
+
+            // Assert
+            Assert.AreEqual(true, jsonString.Contains(actual));
+        }
+
+        [Test]
+        public void ConvertToObject_StronglyTped_GiveBackAStronglyTypeOBject()
+        {
+            // Arrange 
+            var p = new Person { Id = 1, Name = "Name", FirstName = "FirstName" };
+            var c = new Customer { Id = 100, Name = "Customer Name" };
+            p.Customer = c;
+
+            // convert to json
+            var jsonString = Util.ConvertToJson(p);
+
+            // Act
+            var ps = Util.ConvertToObject(jsonString, new Person());
+
+            //Console.WriteLine(string.Format("Id:{0}\n Name:{1}\n CustomerId:{2}\n CustomerName:{3}",
+            //  ps.Id, ps.Name, ps.Customer.Id, ps.Customer.Name));
+
+            // Asssert
+            Assert.AreEqual(ps.Id, p.Id, "Id not the same");
+        }
+    }
+
+
+    /// <summary>
+    /// Test Stubs
+    /// </summary>
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public Customer Customer { get; set; }
+    }
+
+    /// <summary>
+    /// Test Stubs
+    /// </summary>
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
