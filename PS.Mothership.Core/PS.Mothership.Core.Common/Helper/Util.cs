2e12b27f49b90c677943af952b2f4653ff3d65e2
Raj.Manikand <Raj.Manikand@mycompany.com>
2013-09-20 15:05:38 +0000

    util class
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@58 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Helper/Util.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace PS.Mothership.Core.Common.Helper
+{
+    /// <summary>
+    ///     Helper class
+    /// </summary>
+    public sealed class Util
+    {
+        /// <summary>
+        /// Method to convert dynamic object to json string
+        /// </summary>
+        /// <param name="objData"></param>
+        /// <returns></returns>
+        public static string ConvertToJson(dynamic objData)
+        {
+            try
+            {                
+                string json = JsonConvert.SerializeObject(objData, Formatting.None);
+                return json;
+            }
+            catch (Exception ex)
+            {
+                // error logging later
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Convert to dymanic
+        /// </summary>
+        /// <param name="jsonString"></param>
+        /// <returns></returns>
+        public static dynamic ConvertToObject(string jsonString)
+        {
+            try
+            {
+                var obj = JsonConvert.DeserializeObject<dynamic>(jsonString);
+                return obj;
+
+            }
+            catch (Exception ex)
+            {
+                // error logging later
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Convert json string to strong typed object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="jsonString"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static T ConvertToObject<T>(string jsonString, T obj) where T : class
+        {
+            try
+            {
+                var outObject = JsonConvert.DeserializeObject<T>(jsonString);
+                return (T)Convert.ChangeType(outObject, typeof(T), CultureInfo.CurrentCulture);
+            }
+            catch (Exception ex)
+            {
+                // error logging later
+            }
+
+            // return default
+            return default(T);
+        }
+    }
+}
