a14ac81f2bff2f243c1713d1c867eefc45453b7c
Raj.Manikand <Raj.Manikand@mycompany.com>
2013-09-20 15:01:27 +0000

    Cache contracts added
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@57 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/UserProfileDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/UserProfileDto.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 using System.Web.Security;
+using Newtonsoft.Json;
 
 namespace PS.Mothership.Core.Common.Dto
 {
+    /// <summary>
+    /// User Proile Dto
+    /// </summary>
+    /// <remarks>
+    ///     1. For caching purpose, we use the object as cache key, if the client want
+    ///     to ignore certain property which are not any value, decorate those properties
+    ///     with JsonIgnore, as the cache server convert the object to json for generating key
+    /// </remarks>
     public class UserProfileDto
     {
         public bool IsLoggedIn { get; set; }
@@ -17,6 +26,7 @@ namespace PS.Mothership.Core.Common.Dto
         public int TotalCount { get; set; } // gives the total count based on selection criteria
 
         private Guid _sessionId = Guid.NewGuid();
+        [JsonIgnore]
         public Guid SessionId
         {
             get { return _sessionId; }  
