844ec21d3d60ad6ebd019b7812b754abb2e9213c
Raj.Manikand <Raj.Manikand@mycompany.com>
2013-09-24 12:42:39 +0000

    attribute datacontract added to this class for serilization purpose
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@68 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/UserProfileDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/UserProfileDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 using System.Web.Security;
 using Newtonsoft.Json;
 
@@ -12,21 +13,34 @@ namespace PS.Mothership.Core.Common.Dto
     ///     to ignore certain property which are not any value, decorate those properties
     ///     with JsonIgnore, as the cache server convert the object to json for generating key
     /// </remarks>
+    [DataContract]
     public class UserProfileDto
     {
+        [DataMember]
         public bool IsLoggedIn { get; set; }
+        [DataMember]
         public long UserId { get; set; }
+        [DataMember]
         public string UserName { get; set; }
+        [DataMember]
         public bool IsPersistent { get; set; }
+        [DataMember]
         public bool IsValid { get; set; }
+        [DataMember]
         public bool CanImpersonate { get; set; }
+        [DataMember]
         public bool IsImpersonate { get; set; }
+        [DataMember]
         public MembershipCreateStatus Status { get; set; }
+        [DataMember]
         public string Message { get; set; }
+        [DataMember]
         public int TotalCount { get; set; } // gives the total count based on selection criteria
 
+        [NonSerialized]
         private Guid _sessionId = Guid.NewGuid();
-        [JsonIgnore] 
+        [JsonIgnore]
+        [DataMember]
         public Guid SessionId
         {
             get { return _sessionId; }  
