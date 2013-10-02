5da3e0aa78b96d8b8c062126a9f62281a4ad6257
Raj.Manikand <Raj.Manikand@mycompany.com>
2013-10-02 15:29:04 +0000

    MembershipCreateStatus enum create to extend the system.web.security MembershipCreateStatus
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@73 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/AccountDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/AccountDto.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
-using System.Web.Security;
 
 namespace PS.Mothership.Core.Common.Dto
 {
--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/UserProfileDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/UserProfileDto.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Runtime.Serialization;
-using System.Web.Security;
 using Newtonsoft.Json;
 
 namespace PS.Mothership.Core.Common.Dto
