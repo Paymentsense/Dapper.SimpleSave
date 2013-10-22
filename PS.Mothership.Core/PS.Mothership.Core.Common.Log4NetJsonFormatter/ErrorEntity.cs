631196b597ae61956e947fe844c2b40b1163d2ff
sai.krishna <sai.krishna@mycompany.com>
2013-10-22 09:56:49 +0000

    Adding PS.Mothership.Core.Common.Log4netJsonFormatter project to the solution.
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@112 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common.Log4NetJsonFormatter/ErrorEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace PS.Mothership.Core.Common.Log4NetJsonFormatter
+{
+    internal class ErrorEntity
+    {
+        public string Date { get; set; }
+        public string ErrorMessage { get; set; }
+        public string ApplicationName { get; set; }
+        public string Thread { get; set; }
+        public string ErrorLevel { get; set; }
+        public string AssemblyName { get; set; }
+    }
+}
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common.Log4NetJsonFormatter/JsonPatternLayoutConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using log4net.Core;
+using log4net.Layout.Pattern;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace PS.Mothership.Core.Common.Log4NetJsonFormatter
+{
+    public sealed class JsonPatternLayoutConverter : PatternLayoutConverter
+    {
+        public static string AssemblyName { get; set; }
+        // the aim of overriding the convert within the log4net layout is so that stack-traces and multi-line messages
+        // don't break our json format.
+        protected override void Convert(TextWriter writer, LoggingEvent loggingEvent)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            var error = new ErrorEntity 
+                { 
+                    Date = loggingEvent.TimeStamp.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss"),
+                    ApplicationName = loggingEvent.Domain,
+                    AssemblyName = AssemblyName,
+                    ErrorLevel = loggingEvent.Level.ToString(),
+                    ErrorMessage = loggingEvent.RenderedMessage,
+                    Thread = loggingEvent.ThreadName
+                };
+
+            var errorJson = Newtonsoft.Json.JsonConvert.SerializeObject(error, settings);
+            writer.Write(errorJson);
+        }
+    }
+}
