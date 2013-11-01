15d4d5d7b881e8492dfcfe6bd86d68f97f897a9a
sai.krishna <sai.krishna@mycompany.com>
2013-11-01 13:33:25 +0000

    Moved the postcodeanywhere constants to core.
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/LogglyIntegration@136 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Constants/ConfigConstants.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Constants/ConfigConstants.cs
@@ -11,5 +11,9 @@ namespace PS.Mothership.Core.Common.Constants
         //common postcodeanywhere constants
         public const string PostCodeAnywhereAccountCode = "PostCodeAnywhereAccountCode";
         public const string PostCodeAnywhereLicenseKey = "PostCodeAnywhereLicenseKey";
+        public const string PostCodeAnywhereFindUrl = "PostCodeAnywhereFindUrl";
+        public const string PostCodeAnywhereRetrieveUrl = "PostCodeAnywhereRetrieveUrl";
+        public const string PostCodeAnywhereDataType = "PostCodeAnywhereDataType";
+        public const string ForceDisplayExceptionDetailsKey = "ForceDisplayExceptionDetails";
     }
 }
