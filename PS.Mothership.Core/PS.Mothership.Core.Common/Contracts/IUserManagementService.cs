7dd09e322831824071597c7eaf35bb302164cd3e
will.grosett <will.grosett@mycompany.com>
2013-11-04 16:52:48 +0000

    Added to contracts so that roles/groups wcf service is exposed
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/Usermanagement@145 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IUserManagementService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IUserManagementService.cs
@@ -18,6 +18,32 @@ namespace PS.Mothership.Core.Common.Contracts
         /// <param name="userAccountDto"></param>
         /// <returns></returns>
         [OperationContract]
-        UserDto Manage(UserAccountDto userAccountDto);        
+        UserDto Manage(UserAccountDto userAccountDto);
+        [OperationContract]
+        Core.Common.Dto.User GetRolesForUser(int userId);
+        [OperationContract]
+        Core.Common.Dto.UserRoles GetRolesForUsers();
+        [OperationContract]
+        void RemoveRoleFromUser(Core.Common.Dto.UserRole userRole);
+        [OperationContract]
+        void AddRoleToUser(Core.Common.Dto.UserRole userRole);
+        [OperationContract]
+        void RemoveRole(Core.Common.Dto.Role role);
+        [OperationContract]
+        void AddRole(Core.Common.Dto.Role role);
+        [OperationContract]
+        IEnumerable<Core.Common.Dto.Role> GetRoles();
+        [OperationContract]
+        Core.Common.Dto.GroupRoles GetAllRolesAndGroups();
+        [OperationContract]
+        Core.Common.Dto.Group GetRolesForGroup(long groupId);
+        [OperationContract]
+        void RemoveGroup(Core.Common.Dto.GroupDesc groupDesc);
+        [OperationContract]
+        void AddOrUpdateGroup(Core.Common.Dto.GroupDesc groupDesc);
+        [OperationContract]
+        void RemoveRoleFromGroup(Core.Common.Dto.GroupRole groupRole);
+        [OperationContract]
+        void AddRoleToGroup(Core.Common.Dto.GroupRole groupRole);
     }
 }
