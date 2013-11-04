9f45919454ae87ec51bab3e7be3fc7287a30ec0a
will.grosett <will.grosett@mycompany.com>
2013-11-04 17:20:12 +0000

    Added public set property so that it can be serialized!
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/Usermanagement@146 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/GroupRoles.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/GroupRoles.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PS.Mothership.Core.Common.Dto
 {
+    [DataContract]
     public class GroupRoles
     {
         private List<Group> _groups = new List<Group>();
 
-        public List<Group> Groups { get; private set; }
+        [DataMember]
+        public List<Group> Groups { 
+            get{
+                return _groups;
+            }
+            set
+            {
+                _groups = value;
+            }
+        }
 
         public void AddGroup(Group group)
         {
