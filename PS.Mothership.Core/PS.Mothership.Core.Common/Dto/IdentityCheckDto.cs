43ebc678166c0ea16583c656fa59bd1bb8ae97bb
Gregg Kurywchak <gregg.kurywchak@ksfii.com>
2013-11-12 13:06:37 +0000

    Added extra output fields.
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/ExperianAPI_Integration@173 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/IdentityCheckDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/IdentityCheckDto.cs
@@ -36,6 +36,8 @@ namespace PS.Mothership.Core.Common.Dto
         public string ErrorMessage { get; set; }
         [DataMember]
         public byte[] FileBytes { get; set; }
+        [DataMember]
+        public IdentityResultBlock ResultsBlock { get; set; }
     }
 
     [DataContract]
@@ -64,6 +66,8 @@ namespace PS.Mothership.Core.Common.Dto
     {
         [DataMember]
         public string HouseNumber { get; set; }
+        [DataMember]
+        public string HouseName { get; set; }
         /// <summary>
         /// this is the street and must be supplied. You can use any alphanumeric character
         /// </summary>
@@ -104,6 +108,8 @@ namespace PS.Mothership.Core.Common.Dto
     {
         [DataMember]
         public string HouseNumber { get; set; }
+        [DataMember]
+        public string HouseName { get; set; }
         /// <summary>
         /// this is the street and must be supplied. You can use any alphanumeric character
         /// </summary>
@@ -137,4 +143,84 @@ namespace PS.Mothership.Core.Common.Dto
         Male,
         Female,
     }
+
+    [DataContract]
+    public class IdentityResultBlock
+    {
+        [DataMember]
+        public string AuthenticationIndex { get; set; }
+
+        [DataMember]
+        public string AuthIndexText { get; set; }
+
+        [DataMember]
+        public int IDandLocDataAtCLPrimDataItems { get; set; }
+
+        [DataMember]
+        public int IDandLocDataAtCLPrimDataSources { get; set; }
+
+        [DataMember]
+        public string IDandLocDataAtCLStartDateOldestPrim { get; set; }
+
+        [DataMember]
+        public int IDandLocDataAtCLNumSecDataItems { get; set; }
+
+        [DataMember]
+        public int IDandLocDataAtCLNumSecDataSources { get; set; }
+
+        [DataMember]
+        public int LocDataOnlyAtCLocPrimDataItems { get; set; }
+
+        [DataMember]
+        public int LocDataOnlyAtCLocPrimDataSources { get; set; }
+
+        [DataMember]
+        public string LocDataOnlyAtCLocStartDateOldestPrim { get; set; }
+
+        [DataMember]
+        public int LocDataOnlyAtCLocNumSecDataItems { get; set; }
+
+        [DataMember]
+        public int LocDataOnlyAtCLocSecDataSources { get; set; }
+
+        [DataMember]
+        public string LocDataOnlyAtCLocStartDateOldestSec { get; set; }
+
+        [DataMember]
+        public int IDandLocDataAtPLPrimDataItems { get; set; }
+
+        [DataMember]
+        public int IDandLocDataAtPLPrimDataSources { get; set; }
+
+        [DataMember]
+        public int IDandLocDataAtPLSecDataItems { get; set; }
+
+        [DataMember]
+        public int IDandLocDataAtPLSecDataSources { get; set; }
+
+        [DataMember]
+        public int LocDataOnlyAtPLocPrimDataItems { get; set; }
+
+        [DataMember]
+        public int LocDataOnlyAtPLocPrimDataSources { get; set; }
+
+        [DataMember]
+        public int LocDataOnlyAtPLocSecDataItems { get; set; }
+
+        [DataMember]
+        public int LocDataOnlyAtPLocSecDataSources { get; set; }
+
+        [DataMember]
+        public int DataMatchCountsAgeMatchesToPrimSource { get; set; }
+
+        [DataMember]
+        public int DataMatchCountsAgeMatchToSecSource { get; set; }
+
+        [DataMember]
+        public int DataMatchCountsTimeAtCLMtchPrimSource { get; set; }
+
+        [DataMember]
+        public int ReturnedHRPCount { get; set; }
+    }
+
 }
