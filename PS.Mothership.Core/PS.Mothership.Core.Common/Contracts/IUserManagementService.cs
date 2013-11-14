79a7d0e2130edf79ba6be5faac4f8fd689f5bd44
Raj.Manikand <Raj.Manikand@mycompany.com>
2013-11-14 14:11:26 +0000

    method manage removed
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/Usermanagement@183 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IUserManagementService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IUserManagementService.cs
@@ -10,15 +10,7 @@ namespace PS.Mothership.Core.Common.Contracts
 {
     [ServiceContract(Name = "UserManagementService")]
     public interface IUserManagementService
-    {
-        /// <summary>
-        ///     Manage method used to both 
-        ///     add and update a user information
-        /// </summary>
-        /// <param name="userAccountDto"></param>
-        /// <returns></returns>
-        [OperationContract]
-        UserDto Manage(UserAccountDto userAccountDto);
+    {        
         [OperationContract]
         UserDto AddUser(UserProfileDto userProfileDto);
         [OperationContract]
