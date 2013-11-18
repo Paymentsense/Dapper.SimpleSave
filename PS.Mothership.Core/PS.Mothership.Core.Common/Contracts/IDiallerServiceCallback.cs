ebeb241dfd8dbbdd7a3333e25ca7c20529ef2824
Jose Mira <jose.mira@paymentsense.com>
2013-11-18 17:39:29 +0000

    Add DiallerService and DiallerServiceCallback interfaces
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/DiallerWCFServices@188 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IDiallerServiceCallback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Contracts
+{
+    [ServiceContract(Name = "DiallerServiceCallback")]
+    public interface IDiallerServiceCallback
+    {
+    }
+}
