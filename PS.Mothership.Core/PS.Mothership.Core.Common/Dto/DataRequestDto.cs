55f01c6063a5af136ed11fb93b6cab56f4bde1ea
Raj.Manikand <Raj.Manikand@mycompany.com>
2013-11-25 12:02:16 +0000

    DataRequestDto extended to add kendo data source grid information
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/UserGrid&Roles@210 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/DataRequestDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/DataRequestDto.cs
@@ -1,44 +1,185 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.Serialization;
 
 namespace PS.Mothership.Core.Common.Dto
 {
+    /// <summary>
+    ///     This is the mapping of kendo data source options
+    ///     send from the js grid
+    /// </summary>
+    /// <remarks>
+    ///     The naming conventions from javaScript
+    ///     are given the as a data memeber name
+    ///     for easy mapping    
+    /// </remarks>    
     [DataContract]
     public class DataRequestDto
     {
-        [DataMember]
+        [DataMember(Name = "page")]
         public int Page { get; set; }
 
-        [DataMember]
+        [DataMember(Name = "pageSize")]
         public int PageSize { get; set; }
 
         [DataMember]
         public int Start { get; set; }
 
         [DataMember]
-        public int End { get; set; }
+        public int End { get; set; }        
 
         [DataMember]
-        public IList<SortDescriptor> Sorts { get; set; }
+        public IEnumerable<Sort> Sort { get; set; }
+
+        [DataMember]
+        public Filter Filter { get; set; }
     }
-    
+        
     /// <summary>
-    ///     Represents declarative sorting.
+    /// Represents a sort expression of Kendo DataSource.
     /// </summary>
-    public class SortDescriptor
+    [DataContract]
+    public class Sort
     {
-        public SortDescriptor() { }
+        /// <summary>
+        /// Gets or sets the name of the sorted field (property).
+        /// </summary>
+        [DataMember(Name = "field")]
+        public string Field { get; set; }
 
-        public SortDescriptor(string member, ListSortDirection order) { }
+        /// <summary>
+        /// Gets or sets the sort direction. Should be either "asc" or "desc".
+        /// </summary>
+        [DataMember(Name = "dir")]
+        public string Dir { get; set; }
 
-        // Summary:
-        //     Gets or sets the member name which will be used for sorting.
-        public string Member { get; set; }
         //
         // Summary:
         //     Gets or sets the sort direction for this sort descriptor. If the value is
         //     null no sorting will be applied.
-        public ListSortDirection SortDirection { get; set; }
+        public ListSortDirection SortDirection
+        {
+            get
+            {
+                return string.Compare(Dir, "asc", StringComparison.OrdinalIgnoreCase) == 0
+                    ? ListSortDirection.Ascending
+                    : ListSortDirection.Descending;
+            }            
+        }
+
+        /// <summary>
+        /// Converts to form required by Dynamic Linq e.g. "Field1 desc"
+        /// </summary>
+        public string ToExpression()
+        {
+            return Field + " " + Dir;
+        }
+    }
+
+    /// <summary>
+    /// Represents a filter expression of Kendo DataSource.
+    /// </summary>
+    [DataContract]
+    public class Filter
+    {
+        /// <summary>
+        /// Gets or sets the name of the sorted field (property). Set to <c>null</c> if the <c>Filters</c> property is set.
+        /// </summary>
+        [DataMember(Name = "field")]
+        public string Field { get; set; }
+
+        /// <summary>
+        /// Gets or sets the filtering operator. Set to <c>null</c> if the <c>Filters</c> property is set.
+        /// </summary>
+        [DataMember(Name = "operator")]
+        public string Operator { get; set; }
+
+        /// <summary>
+        /// Gets or sets the filtering value. Set to <c>null</c> if the <c>Filters</c> property is set.
+        /// </summary>
+        [DataMember(Name = "value")]
+        public object Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the filtering logic. Can be set to "or" or "and". Set to <c>null</c> unless <c>Filters</c> is set.
+        /// </summary>
+        [DataMember(Name = "logic")]
+        public string Logic { get; set; }
+
+        /// <summary>
+        /// Gets or sets the child filter expressions. Set to <c>null</c> if there are no child expressions.
+        /// </summary>
+        [DataMember(Name = "filters")]
+        public IEnumerable<Filter> Filters { get; set; }
+
+        /// <summary>
+        /// Mapping of Kendo DataSource filtering operators to Dynamic Linq
+        /// </summary>
+        private static readonly IDictionary<string, string> Operators = new Dictionary<string, string>
+        {
+            {"eq", "="},
+            {"neq", "!="},
+            {"lt", "<"},
+            {"lte", "<="},
+            {"gt", ">"},
+            {"gte", ">="},
+            {"startswith", "StartsWith"},
+            {"endswith", "EndsWith"},
+            {"contains", "Contains"}
+        };
+
+        /// <summary>
+        /// Get a flattened list of all child filter expressions.
+        /// </summary>
+        public IList<Filter> All()
+        {
+            var filters = new List<Filter>();
+
+            Collect(filters);
+
+            return filters;
+        }
+
+        private void Collect(IList<Filter> filters)
+        {
+            if (Filters != null && Filters.Any())
+            {
+                foreach (Filter filter in Filters)
+                {
+                    filters.Add(filter);
+
+                    filter.Collect(filters);
+                }
+            }
+            else
+            {
+                filters.Add(this);
+            }
+        }
+
+        /// <summary>
+        /// Converts the filter expression to a predicate suitable for Dynamic Linq e.g. "Field1 = @1 and Field2.Contains(@2)"
+        /// </summary>
+        /// <param name="filters">A list of flattened filters.</param>
+        public string ToExpression(IList<Filter> filters)
+        {
+            if (Filters != null && Filters.Any())
+            {
+                return "(" + String.Join(" " + Logic + " ", Filters.Select(filter => filter.ToExpression(filters)).ToArray()) + ")";
+            }
+
+            int index = filters.IndexOf(this);
+
+            string comparison = Operators[Operator];
+
+            if (comparison == "StartsWith" || comparison == "EndsWith" || comparison == "Contains")
+            {
+                return String.Format("{0}.{1}(@{2})", Field, comparison, index);
+            }
+
+            return String.Format("{0} {1} @{2}", Field, comparison, index);
+        }
     }
 }
