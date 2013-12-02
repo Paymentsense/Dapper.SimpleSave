fed233410de4ba7e83e2b606b4eaccc089c88443
Jose Mira <jose.mira@paymentsense.com>
2013-12-02 14:04:13 +0000

    Addition of IDiallerTaskNotificationService and IDiallerTaskNotificationCallback
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/DiallerService@231 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IDiallerTaskNotificationCallback.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using PS.Mothership.Core.Common.Dto;
+
+namespace PS.Mothership.Core.Common.Contracts
+{
+    [ServiceContract]
+    public interface IDiallerTaskNotificationCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void ReceiveNotification(TaskDto taskDto);
+    }
+}
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IDiallerTaskNotificationService.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceModel;
+using PS.Mothership.Core.Common.Dto;
+
+namespace PS.Mothership.Core.Common.Contracts
+{
+    [ServiceContract(CallbackContract = typeof(IDiallerTaskNotificationCallback))]
+    public interface IDiallerTaskNotificationService : IQuartzJobBase
+    {
+        [OperationContract(IsOneWay = false)]
+        void Subscribe(string applicationName);
+        [OperationContract(IsOneWay = true)]
+        void EndSubscribe(string applicationName);
+        [OperationContract]
+        void AddNewTask(TaskDto taskDto);
+        [OperationContract]
+        void UpdateTask(TaskDto taskDto);
+        [OperationContract]
+        void CompleteTask(long pendingNotificationId);
+        [OperationContract]
+        void PushPendingTaskNotifications(string userName);
+    }
+}
