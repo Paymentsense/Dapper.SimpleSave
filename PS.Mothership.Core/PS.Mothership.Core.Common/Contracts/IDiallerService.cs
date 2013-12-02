d0fc759bcaf51631d852c93a9cbc7c51afc62175
Jose Mira <jose.mira@paymentsense.com>
2013-12-02 12:35:02 +0000

    Changes due to information flow restructure
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/DiallerService@229 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IDiallerService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IDiallerService.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ServiceModel;
 using PS.Mothership.Core.Common.Dto;
-using PS.Mothership.Core.Common.Template.Dial;
 
 namespace PS.Mothership.Core.Common.Contracts
 {
-    [ServiceContract(CallbackContract = typeof (IDiallerServiceCallback), Name = "DiallerService")]
+    [ServiceContract(Name = "DiallerService")]
     public interface IDiallerService
     {
         [OperationContract]
@@ -18,46 +18,13 @@ namespace PS.Mothership.Core.Common.Contracts
         Guid LogDiallerSessionUnsubscribe(DiallerSessionUnsubscribeDto diallerSessionUnsubscribe);
 
         [OperationContract]
-        bool UpdateSelectedSalesUserAndCampaign(SelectedSalesUserAndCampaignDto selectedSalesUserAndCampaign);
-
-        [OperationContract]
         SipAccountDetailsDto GetSipAccountDetails(Guid userGuid);
 
-        [OperationContract]
-        long LogClientModeChange(ClientModeChangeDto clientModeChange);
-
-        [OperationContract]
-        GetMerchantByPhoneDto GetMerchantByPhone(string phoneNumber);
-
-        [OperationContract]
-        CampaignDetailsDto GetCampaignDetailsByCalledNumber(string calledNumber);
-
-        [OperationContract]
-        long? AddInboundQueueCallRecord(InboundQueueCallRecordDto inboundQueueCallRecord);
-
-        [OperationContract]
-        string TryToFindMerchantByGuid(Guid merchantGuid);
-
-        [OperationContract]
-        void LogFinalisedCall(DiallerCallDto diallerCall);
+        [OperationContract(IsOneWay = true)]
+        List<InboundQueueDetailsDto> GetInboundQueueDetails();
 
         [OperationContract(IsOneWay = true)]
-        void GetNextCallQueue(Guid agentUserGuid);
-
-        [OperationContract]
-        void SetInboundQueueCallRecordResolution(long inboundQueueCallTrnId, InboundQueueCallResolutionEnum resolution);
-
-        [OperationContract(IsOneWay = true)]
-        void GetInboundQueueDetails();
-
-        [OperationContract(IsOneWay = true)]
-        void GetUserSalesUsers(Guid userGuid);
-
-        [OperationContract]
-        bool IsCallResolved(long pcMerchantTranId);
-
-        [OperationContract(IsOneWay = true)]
-        void GetMissingCallRecordings(DateTime dateStart, DateTime dateEnd);
+        List<MissingCallRecordingsDto> GetMissingCallRecordings(DateTime dateStart, DateTime dateEnd);
 
         [OperationContract]
         void UpdateRecorderCallIdForCallGuid(Guid callGuid, int recorderCallId, Guid userGuid);
