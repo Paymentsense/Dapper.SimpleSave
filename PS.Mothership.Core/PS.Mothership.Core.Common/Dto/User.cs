8f871c511b58b6ee4c2ddff0156d9c296d675c5a
will.grosett <will.grosett@mycompany.com>
2013-12-11 15:27:44 +0000

    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/Permissions&Cleanup@266 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/User.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/User.cs
@@ -11,7 +11,6 @@ namespace PS.Mothership.Core.Common.Dto
         public Guid UserGuid { get; set; }
         public long Id { get; set; }
         public string Name { get; set; }
-        public List<Role> AssignedRoles { get; set; }
-        public List<Role> AvailableRoles { get; set; }
+        public List<UserAssignedAndAvailableRole> Roles { get; set; }
     }
 }
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/UserAssignedAndAvailableRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Dto
+{
+    public class UserAssignedAndAvailableRole : Role
+    {
+        public bool AssignedToUser { get; set; }
+    }
+}
