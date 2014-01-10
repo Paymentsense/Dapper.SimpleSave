8c55a257a32eb3caa466eb6ae1c1da8da39f83f1
Jose Mira <jose.mira@paymentsense.com>
2014-01-10 09:49:34 +0000

    Update templated enums
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/DiallerService@315 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common.Template/Template/CommMessageServiceStatusEnum.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Runtime.Serialization;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace PS.Mothership.Core.Common.Template.Comm
+{
+    
+    [DataContract]
+    public enum MessageServiceStatusEnum : long
+    {
+       [Description("")][EnumMember]Normal = 1,
+       [Description("")][EnumMember]Unstable = 2,
+       [Description("")][EnumMember]Errored = 3,
+        
+    }
+
+    public class MessageServiceStatus
+    {
+        public long EnumValue {get;set;}
+		public string EnumName {get;set;}
+		public string EnumDescription {get;set;}
+		public long ServiceStatusKey {get;set;}
+		public bool IsUseable {get;set;}
+    }
+
+    /// <summary>
+    /// This class is mainly for using the extended properties of Enum
+    /// </summary>
+    public static class MessageServiceStatusCollection
+    {
+        private static List<MessageServiceStatus> _list; 
+        public static List<MessageServiceStatus> MessageServiceStatusList
+        {
+            get
+            {
+                if (_list == null)
+                {   
+                        _list = new List<MessageServiceStatus>
+                        {
+                            new MessageServiceStatus
+							{
+								EnumValue = 1,
+								EnumName = "Normal",
+								EnumDescription = "",
+								ServiceStatusKey = 1,
+								IsUseable = true
+							},
+							new MessageServiceStatus
+							{
+								EnumValue = 2,
+								EnumName = "Unstable",
+								EnumDescription = "",
+								ServiceStatusKey = 2,
+								IsUseable = true
+							},
+							new MessageServiceStatus
+							{
+								EnumValue = 3,
+								EnumName = "Errored",
+								EnumDescription = "",
+								ServiceStatusKey = 3,
+								IsUseable = true
+							},
+                        };
+                    
+                }
+                return _list;
+            }
+
+        }
+    }
+   
+}
