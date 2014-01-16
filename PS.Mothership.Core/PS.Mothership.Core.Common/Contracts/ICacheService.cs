a24997990b5bc57d08dfd6a262987d7768398118
Raj.Manikand <Raj.Manikand@mycompany.com>
2014-01-16 10:03:05 +0000

    method added to the ICacheService to support removal of cache data
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/Permissions&Cleanup@323 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/ICacheService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/ICacheService.cs
@@ -26,5 +26,14 @@ namespace PS.Mothership.Core.Common.Contracts
         /// <param name="tags">search tag, should be of DataCacheTag, the client implmenting it should make sure it's of type DataCacheTag, 
         /// this is supported only in AppFabric </param>
         void PutData<T>(string cacheKey, T type, TimeSpan? timeout = null, IEnumerable<T> tags=null);
+
+        /// <summary>
+        ///     Remove date from cache    
+        /// </summary>
+        /// <remarks>
+        ///     The client should give a valid key
+        /// </remarks>
+        /// <param name="cacheKey">cache key</param>
+        void Remove(string cacheKey);
     }
 }
