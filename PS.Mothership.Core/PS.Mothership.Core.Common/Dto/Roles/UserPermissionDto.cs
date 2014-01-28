b6cbcce0a74740c114ff3f72153f4cbfbb6e1372
Raj.Manikand <Raj.Manikand@mycompany.com>
2014-01-28 16:45:44 +0000

    classess moved to separate file - refactor
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/Permissions&Cleanup@363 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Roles/UserPermissionDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Template.Usr;
+
+namespace PS.Mothership.Core.Common.Dto.Roles
+{
+    [DataContract]
+    [Obsolete]
+    public class UserPermissionDto
+    {
+        [DataMember]
+        public UserPermissionFlagEnum PermissionEnums { get; set; }        
+
+        [DataMember]
+        public string PermissionName { get; set; }
+
+        [DataMember]
+        public UserRoleDto Role { get; set; }        
+    }
+}
--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Roles/UserRoleDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Roles/UserRoleDto.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using PS.Mothership.Core.Common.Template.Usr;
+﻿using System.Runtime.Serialization;
 
 namespace PS.Mothership.Core.Common.Dto.Roles
-{
-    // TODO: Refactor
-
+{    
     [DataContract]
     public class UserRoleDto
     {
@@ -19,44 +14,4 @@ namespace PS.Mothership.Core.Common.Dto.Roles
         [DataMember]
         public bool IsDefining { get; set; }
     }
-
-    [DataContract]
-    public class UserGroupDto
-    {
-        [DataMember]
-        public long GroupId { get; set; }
-
-        [DataMember]
-        public string GroupName { get; set; }
-
-        [DataMember]
-        public List<UserRoleDto> Role { get; set; }
-    }
-
-    [DataContract]
-    [Obsolete]
-    public class UserPermissionDto
-    {
-        [DataMember]
-        public UserPermissionFlagEnum PermissionEnums { get; set; }        
-
-        [DataMember]
-        public string PermissionName { get; set; }
-
-        [DataMember]
-        public UserRoleDto Role { get; set; }        
-    }
-
-    [DataContract]
-    public class ResourceRolePermissionsDto
-    {
-        [DataMember]
-        public ResourceEnum ResourceEnum { get; set; }        
-
-        [DataMember]
-        public UserRoleDto Role { get; set; }
-        
-        [DataMember]
-        public UserPermissionFlagEnum PermissionEnums { get; set; }        
-    }    
 }
