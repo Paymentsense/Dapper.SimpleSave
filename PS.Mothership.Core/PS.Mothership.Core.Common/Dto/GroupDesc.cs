71ba329e5ff2a87aa44f53556f5f3f6706187abb
Jose Mira <jose.mira@paymentsense.com>
2014-01-31 17:01:07 +0000

    cler unused classed from repo
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/DiallerService@376 0f896bb5-3ab0-2d4e-82a7-85ef10020915

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/GroupDesc.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PS.Mothership.Core.Common.Dto
-{
-    public class GroupDesc
-    {
-        public long Id { get; set; }
-        public string GroupName { get; set; }
-        public string GroupDescription { get; set; }
-    }
-}
