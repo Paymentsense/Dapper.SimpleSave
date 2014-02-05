618e2ab1ba76eac9731f6ca57bd946668935c8dc
Sam Bullock <sam.bullock@paymentsense.com>
2014-02-05 10:59:54 +0000

    Merged revision(s) 370-387 from branches/Permissions&Cleanup:
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/trunk@389 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Template/CommMessageDirectionEnum.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Runtime.Serialization;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace PS.Mothership.Core.Common.Template.Comm
+{
+    
+    [DataContract]
+    public enum MessageDirectionEnum : long
+    {
+       [Description("")][EnumMember]Blank = 1,
+       [Description("")][EnumMember]Sent = 2,
+       [Description("")][EnumMember]Received = 3,
+        
+    }
+
+    public class MessageDirection
+    {
+        public long EnumValue {get;set;}
+		public string EnumName {get;set;}
+		public string EnumDescription {get;set;}
+		public long MessageDirectionKey {get;set;}
+    }
+
+    /// <summary>
+    /// This class is mainly for using the extended properties of Enum
+    /// </summary>
+    public static class MessageDirectionCollection
+    {
+        private static List<MessageDirection> _list; 
+        public static List<MessageDirection> MessageDirectionList
+        {
+            get
+            {
+                if (_list == null)
+                {   
+                        _list = new List<MessageDirection>
+                        {
+                            new MessageDirection
+							{
+								EnumValue = 1,
+								EnumName = "Blank",
+								EnumDescription = "",
+								MessageDirectionKey = 1
+							},
+							new MessageDirection
+							{
+								EnumValue = 2,
+								EnumName = "Sent",
+								EnumDescription = "",
+								MessageDirectionKey = 2
+							},
+							new MessageDirection
+							{
+								EnumValue = 3,
+								EnumName = "Received",
+								EnumDescription = "",
+								MessageDirectionKey = 3
+							},
+                        };
+                    
+                }
+                return _list;
+            }
+
+        }
+    }
+   
+}
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Template/CommMessageEmailAddresstypeEnum.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Runtime.Serialization;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace PS.Mothership.Core.Common.Template.Comm
+{
+    
+    [DataContract]
+    public enum MessageEmailAddresstypeEnum : long
+    {
+       [Description("")][EnumMember]Blank = 1,
+       [Description("")][EnumMember]From = 2,
+       [Description("")][EnumMember]To = 3,
+       [Description("")][EnumMember]CC = 4,
+       [Description("")][EnumMember]BCC = 5,
+        
+    }
+
+    public class MessageEmailAddresstype
+    {
+        public long EnumValue {get;set;}
+		public string EnumName {get;set;}
+		public string EnumDescription {get;set;}
+		public long MessageEmailAddressTypeKey {get;set;}
+    }
+
+    /// <summary>
+    /// This class is mainly for using the extended properties of Enum
+    /// </summary>
+    public static class MessageEmailAddresstypeCollection
+    {
+        private static List<MessageEmailAddresstype> _list; 
+        public static List<MessageEmailAddresstype> MessageEmailAddresstypeList
+        {
+            get
+            {
+                if (_list == null)
+                {   
+                        _list = new List<MessageEmailAddresstype>
+                        {
+                            new MessageEmailAddresstype
+							{
+								EnumValue = 1,
+								EnumName = "Blank",
+								EnumDescription = "",
+								MessageEmailAddressTypeKey = 1
+							},
+							new MessageEmailAddresstype
+							{
+								EnumValue = 2,
+								EnumName = "From",
+								EnumDescription = "",
+								MessageEmailAddressTypeKey = 2
+							},
+							new MessageEmailAddresstype
+							{
+								EnumValue = 3,
+								EnumName = "To",
+								EnumDescription = "",
+								MessageEmailAddressTypeKey = 3
+							},
+							new MessageEmailAddresstype
+							{
+								EnumValue = 4,
+								EnumName = "CC",
+								EnumDescription = "",
+								MessageEmailAddressTypeKey = 4
+							},
+							new MessageEmailAddresstype
+							{
+								EnumValue = 5,
+								EnumName = "BCC",
+								EnumDescription = "",
+								MessageEmailAddressTypeKey = 5
+							},
+                        };
+                    
+                }
+                return _list;
+            }
+
+        }
+    }
+   
+}
