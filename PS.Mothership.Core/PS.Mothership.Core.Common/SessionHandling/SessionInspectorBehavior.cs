060092850dec5569ca90742ad79dab7e6b26480b
Vipin Vijay <vipin.vijay@paymentsense.com>
2014-04-10 10:40:38 +0000

    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/RepositoryRefactoring@588 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/SessionHandling/SessionInspectorBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace PS.Mothership.Core.Common.SessionHandling
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SessionInspectorBehavior : Attribute, IDispatchMessageInspector
+    {
+        #region IDispatchMessageInspector
+
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            var header = request.Headers.GetHeader<SessionHeader>("session-header", "s");
+
+
+            return null;
+        }
+
+        public void BeforeSendReply(ref Message reply, object correlationState)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/SessionHandling/SessionInspectorBehaviorExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel.Configuration;
+
+namespace PS.Mothership.Core.Common.SessionHandling
+{
+
+    public class SessionInspectorBehaviorExtension : BehaviorExtensionElement
+    {
+        protected override object CreateBehavior()
+        {
+            return new SessionInspectorBehavior();
+        }
+
+        public override Type BehaviorType
+        {
+            get { return typeof(SessionInspectorBehavior); }
+        }
+    }
+}
