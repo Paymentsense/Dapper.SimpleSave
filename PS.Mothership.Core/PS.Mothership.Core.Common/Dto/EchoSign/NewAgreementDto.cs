95ffddd68adc5acda957355f88888c5989bbeda4
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-04-15 12:00:20 +0000

    Created EchoSign DTOs
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/EchoSign@609 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/EchoSign/NewAgreementDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Dto.EchoSign
+{
+    [DataContract]
+    public class NewAgreementDto
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public string RecipientsEmail { get; set; }
+
+        [DataMember]
+        public string RecipientsRole { get; set; }
+
+        [DataMember]
+        public List<MergeFieldInfoDto> MergefieldInfo { get; set; }
+    }
+}
