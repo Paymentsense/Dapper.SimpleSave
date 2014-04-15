3cf65bab9fee8ac2a3201e911f4f5a09ab2819f4
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-04-15 15:35:54 +0000

    Created EchoSign DTOs
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/EchoSign@611 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/EchoSign/AppCredentialsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Dto.EchoSign
+{
+    [DataContract]
+    public class AppCredentialsDto
+    {
+        [DataMember]
+        public string ApplicationId { get; set; }
+
+        [DataMember]
+        public string ApplicationSecret { get; set; }
+    }
+}
