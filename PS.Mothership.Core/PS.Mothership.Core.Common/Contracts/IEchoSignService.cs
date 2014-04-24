b3e3828593dce2c2d82dd033d2a3eff11c3f6c00
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-04-24 09:53:42 +0000

    IEchoSignService created
    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/EchoSign@629 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IEchoSignService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Contracts
+{
+    [ServiceContract]
+    public interface IEchoSignService
+    {
+        [OperationContract]
+        void DocumentNotification(string documentKey);
+    }
+}
