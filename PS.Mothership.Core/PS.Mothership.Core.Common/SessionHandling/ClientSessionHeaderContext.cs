023ab96f6d428c848eb73e033af6a888c5841296
Vipin Vijay <vipin.vijay@paymentsense.com>
2014-04-25 15:23:42 +0000

    git-svn-id: https://svn.mothershipdev.com:9443/svn/PaymentSense_MothershipV2_Core/branches/RepositoryRefactoring@636 0f896bb5-3ab0-2d4e-82a7-85ef10020915

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/SessionHandling/ClientSessionHeaderContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using PS.Mothership.Core.Common.Dto;
+
+namespace PS.Mothership.Core.Common.SessionHandling
+{
+    public interface IClientSessionHeaderContext
+    {
+        SessionHeader SessionInformation { get; set; }
+    }
+
+    public class ClientSessionHeaderContext : IClientSessionHeaderContext
+    {
+        public SessionHeader SessionInformation { get; set; }
+
+        public ClientSessionHeaderContext()
+        {
+            SessionInformation = new SessionHeader();
+        }
+    }
+}
