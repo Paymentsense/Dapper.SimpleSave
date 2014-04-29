127668b85cf3651b60e9252404f9702d7fdffc33
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-04-29 14:41:11 +0100

    Squashed 'Externals/Core/' changes from 2ded869..c23471c
    c23471c Removed unused directives from EchoSign
    
    git-subtree-dir: Externals/Core
    git-subtree-split: c23471cb4dc7996f4db7477c2355b69dc9309791

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IEchoSignCallbackService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IEchoSignCallbackService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceModel;
 using PS.Mothership.Core.Common.Enums.EchoSign;
 
 namespace PS.Mothership.Core.Common.Contracts
