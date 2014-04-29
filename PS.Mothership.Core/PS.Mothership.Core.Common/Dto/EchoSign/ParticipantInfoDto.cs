a0db6ab817e7bc36e18c62198cc7f72c577b810d
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-04-29 14:24:56 +0100

    Removed unused directives from EchoSign

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/EchoSign/ParticipantInfoDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/EchoSign/ParticipantInfoDto.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 using PS.Mothership.Core.Common.Enums.EchoSign;
 
 namespace PS.Mothership.Core.Common.Dto.EchoSign
