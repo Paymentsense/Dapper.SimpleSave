b66f576fa55a7e2228dbd23be01b6e5129c499bc
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-04-30 11:45:02 +0100

    Squashed 'Externals/Core/' changes from c23471c..60b4737
    60b4737 create reminder emum
    c537f64 add constants
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 60b47370be5ca8f8b91c48b4ed84d12245ca6f61

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Enums/EchoSign/ReminderFrequencyEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace PS.Mothership.Core.Common.Enums.EchoSign
+{
+    [DataContract]
+    public enum ReminderFrequencyEnum
+    {
+        [EnumMember]
+        DAILY_UNTIL_SIGNED,
+        [EnumMember]
+        WEEKLY_UNTIL_SIGNED
+    }
+}
