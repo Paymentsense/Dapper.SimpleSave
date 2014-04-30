1f9f28a99f5dbce02c57feaf18bdef7737285352
Vipin Vijay <vipin.vijay@paymentsense.com>
2014-04-30 15:38:32 +0100

    Commit of core at a stage where Service is running

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/User/UserGroupDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/User/UserGroupDto.cs
@@ -5,16 +5,16 @@ using PS.Mothership.Core.Common.Dto.Roles;
 
 namespace PS.Mothership.Core.Common.Dto.User
 {
-    [DataContract]
-    public class UserGroupDto
-    {
-        [DataMember]
-        public Guid GroupGuid { get; set; }
+    //[DataContract]
+    //public class UserGroupDto
+    //{
+    //    [DataMember]
+    //    public Guid GroupGuid { get; set; }
 
-        [DataMember]
-        public string GroupName { get; set; }
+    //    [DataMember]
+    //    public string GroupName { get; set; }
 
-        [DataMember]
-        public List<UserRoleDto> Role { get; set; }
-    }
+    //    [DataMember]
+    //    public List<UserRoleDto> Role { get; set; }
+    //}
 }
