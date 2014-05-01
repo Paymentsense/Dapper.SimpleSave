8acae20fe9ccb416adfb7afaff55314db84c8718
Vipin Vijay <vipin.vijay@paymentsense.com>
2014-05-01 17:53:20 +0100

    Squashed 'Externals/Core/' changes from a62363a..02ada25
    02ada25 Part of Repository Refactoring
    2d0ae28 Part of Repository Refactoring
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 02ada253e4735644778db34fc9747c7b50d5582d

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/SessionHandling/SessionDispatchInspector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace PS.Mothership.Core.Common.SessionHandling
+{
+    public class SessionDispatchInspector : IDispatchMessageInspector
+    {
+        private readonly ISessionManager _sessionManager;
+
+        public SessionDispatchInspector(ISessionManager sessionManager)
+        {
+            _sessionManager = sessionManager;
+        }
+
+
+        #region IDispatchMessageInspector
+
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            var header = request.Headers.GetHeader<SessionHeader>("session-header", "s");
+            //SessionOperationContextExtension.Current.Session.WebSessionGuid = header != null ?  header.WebSessionGuid : Guid.Empty;
+            _sessionManager.SessionInformation.WebSessionGuid = header != null ?  header.WebSessionGuid : Guid.Empty;
+            return null;
+        }
+
+        public void BeforeSendReply(ref Message reply, object correlationState)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+       
+    }
+}
