41516a68b94d48e352f4d15da2ce185ade6c39fe
David Welch <david.welch@paymentsense.com>
2014-05-28 11:10:36 +0100

    Squashed 'Externals/Core/' changes from 50e9486..4a62aa9
    4a62aa9 Renamed some files and moved contacts to merchants.
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 4a62aa917686b9bd4e8e8131547eec43dde5979c

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IProspectCustomerService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IProspectCustomerService.cs
@@ -4,21 +4,24 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-using PS.Mothership.Core.Common.Dto.Customer;
+using PS.Mothership.Core.Common.Dto.Merchant;
 
 namespace PS.Mothership.Core.Common.Contracts
 {
     [ServiceContract(Name = "ProspectCustomerService")]
     public interface IProspectCustomerService
     {
+        [OperationContract]
+        ProspectDto GetProspect(Guid id);
+
         /// <summary>
         ///     Add customer
         /// </summary>
         /// <param name="prospectDto"></param>
-       /// <param name="prospectDetailsDto"></param>
+        /// <param name="prospectDto"></param>
         /// <returns></returns>
         [OperationContract]
-        ProspectResponseDto AddCustomer(ProspectDetailsDto prospectDetailsDto);
+        ProspectResponseDto AddProspect(ProspectDto prospectDto);
 
         /// <summary>
         ///     Get similiar customers
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/CustomerDetailsDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Dto.Contact;
+using PS.Mothership.Core.Common.Dto.Message;
+
+namespace PS.Mothership.Core.Common.Dto.Merchant
+{
+    [DataContract]
+    public class CustomerDetailsDto 
+    {
+        [DataMember]
+        public CustomerDto Customer { get; set; }
+
+        [DataMember]
+        public ContactDto Contact { get; set; }
+
+        [DataMember]
+        public PhoneNumberDto Phone { get; set; }
+
+        [DataMember]
+        public EmailAddressDto Email { get; set; }
+    }
+}
