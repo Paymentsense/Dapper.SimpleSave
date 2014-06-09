95f1e3053d980ad6c929f833ccd73d6e029b8e4d
José Mira <jose.mira@paymentsense.com>
2014-06-09 17:13:23 +0100

    Squashed 'Externals/Core/' changes from 7293552..3346ec2
    3346ec2 Code cleanup Changed from using List<T> to IList<T>
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 3346ec26c5909b1235d57af462d16a4ccc88cac2

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Dialler/FinalisedDiallerCallDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Dialler/FinalisedDiallerCallDto.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using PS.Mothership.Core.Common.Template.Dial;
+using System;
 using System.Runtime.Serialization;
-using PS.Mothership.Core.Common.Template.Dial;
 
 namespace PS.Mothership.Core.Common.Dto.Dialler
 {
@@ -9,34 +9,24 @@ namespace PS.Mothership.Core.Common.Dto.Dialler
     {
         [DataMember]
         public Guid SipCallGuid { get; set; }
-
         [DataMember]
         public DateTimeOffset EndDateTime { get; set; }
-
         [DataMember]
         public string TransferredNumber { get; set; }
-
         [DataMember]
         public DialCallDispositionEnum CallDisposition { get; set; }
-
         [DataMember]
         public int PreviewTime { get; set; }
-
         [DataMember]
         public int DialOrRingTime { get; set; }
-
         [DataMember]
         public int TalkTime { get; set; }
-
         [DataMember]
         public int HoldTime { get; set; }
-
         [DataMember]
         public int WrapTime { get; set; }
-
         [DataMember]
         public long? RecorderCallId { get; set; }
-
         [DataMember]
         public Guid? MerchantGuid { get; set; }
     }
