85a6bd5c872da08886c9091a9a9057d5662a69aa
David Welch <david.welch@paymentsense.com>
2014-06-11 09:57:01 +0100

    Merge commit '0edf1272fc6a4279ecb091d78823831b56e6600b' into MerchantChanges

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/ProspectAddressDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/ProspectAddressDto.cs
@@ -1,16 +1,15 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
 using System.Runtime.Serialization;
 
 
 namespace PS.Mothership.Core.Common.Dto.Merchant
 {
     [DataContract]
-    public class ProspectAddressDto : MerchantDto
+    public class ProspectAddressDto
     {
+        [DataMember]
+        public Guid MerchantGuid { get; set; }
+
         [DataMember]
         public string BusinessName { get; set; }
 
