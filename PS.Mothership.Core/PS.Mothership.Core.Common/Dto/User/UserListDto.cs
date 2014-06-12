b19e5da4355979d32e6c194bb2dd1f1a046d9f68
David Welch <david.welch@paymentsense.com>
2014-06-12 09:26:31 +0100

    Merge commit '3ed5e87852386c12685acb4815104561ae4556b5' into MerchantChanges

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/User/UserListDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PS.Mothership.Core.Common.Dto.User
+{
+    public class UserListDto
+    {
+        public Template.Usr.UsrLoginResultEnum Status { get; set; }
+
+        public Guid UserGuid { get; set; }
+
+        public bool CanImpersonate { get; set; }
+
+        public Template.Usr.UsrStatusOptionFlagEnum StatusOptions { get; set; }
+
+        public string LoginName { get; set; }
+
+        public string UserName { get; set; }
+
+        public bool IsDefining { get; set; }
+
+        public Template.Usr.UsrUserStatusEnum UserStatus { get; set; }
+
+        public string Email { get; set; }
+
+        public Template.Usr.UsrUserTypeEnum UserType { get; set; }
+
+        public Guid SipAccountGuid { get; set; }
+
+        public string DefiningRoleDescription { get; set; }
+    }
+}
