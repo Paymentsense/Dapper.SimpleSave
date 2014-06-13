1bd59f8420b5d46ae67672be3e2530f365e43368
David Welch <david.welch@paymentsense.com>
2014-06-13 09:01:55 +0100

    Merge commit 'd4d552e49a561f8ef135a27dd2517456387bc82c' into MerchantChanges

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/User/UserListDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/User/UserListDto.cs
@@ -1,21 +1,60 @@
-﻿using System;
+﻿using PS.Mothership.Core.Common.Dto.Groups;
+using PS.Mothership.Core.Common.Dto.Roles;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace PS.Mothership.Core.Common.Dto.User
 {
+    [DataContract]
     public class UserListDto
     {
+        [DataMember]
         public Guid UserGuid { get; set; }
 
+        [DataMember]
         public Template.Usr.UsrStatusOptionFlagEnum StatusOptions { get; set; }
 
+        [DataMember]
         public string LoginName { get; set; }
 
+        [DataMember]
         public string UserName { get; set; }
 
+        [DataMember]
         public Template.Usr.UsrUserStatusEnum UserStatus { get; set; }
-        
+
+        [DataMember]
         public Template.Usr.UsrUserTypeEnum UserType { get; set; }
-        
-        public string DefiningRoleDescription { get; set; }
+
+        [DataMember]
+        public ICollection<RoleDto> Roles { get; set; }
+
+        [DataMember]
+        public ICollection<GroupDto> Groups { get; set; }
+
+        public GroupDto DefiningGroup
+        {
+            get
+            {
+                return
+                    (from g in Groups
+                     from r in g.Roles
+                     where r.RoleGuid == DefiningRole.RoleGuid
+                     select g).FirstOrDefault();
+            }
+        }
+
+        public RoleDto DefiningRole
+        {
+            get
+            {
+                return Roles.Union(Groups.SelectMany(x => x.Roles)).FirstOrDefault(x => x.IsDefining);
+            }
+        }
+
+        [DataMember]
+        public Guid SipAccountGuid { get; set; }
     }
 }
