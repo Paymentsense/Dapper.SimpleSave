031e9419f5e1d0ea5b38c76e0f0bd964a87dabaf
Guy Butler <guy.butler@paymentsense.com>
2014-06-16 15:03:17 +0100

    Added a temporary integration test project Integration Tests for Json and Xml Uri services Renaming some components/models dates for xml and json had to be made the same

--- PS.Mothership.Core/PS.Mothership.Core.Common/Constants/ConfigConstants.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Constants/ConfigConstants.cs
@@ -95,9 +95,9 @@
 		// Companies House Constants
 		public const string CompaniesHouseGatewayUserId = "CompaniesHouseGatewayUserId";
 		public const string CompaniesHouseGatewayPassword = "CompaniesHouseGatewayPassword";
-        public const string CompaniesHouseXmlWebUrl = "XmlWebUrl";
-        public const string CompaniesHouseJsonWebUrl = "JsonWebUrl";
-        public const string ComapniesHouseGatewayEndpoint = "GatewayEndpoint";
+        public const string CompaniesHouseXmlWebUrl = "CompaniesHouseXmlWebUrl";
+        public const string CompaniesHouseJsonWebUrl = "CompaniesHouseJsonWebUrl";
+        public const string ComapniesHouseGatewayEndpoint = "CompaniesHouseGatewayEndpoint";
         public const string CompaniesHouseWebRequestTimeoutMilliseconds = "CompaniesHouseWebRequestTimeoutMilliseconds";
         public const string CompaniesHouseMaxRetryCount = "CompaniesHouseMaxRetryCount";
     }
