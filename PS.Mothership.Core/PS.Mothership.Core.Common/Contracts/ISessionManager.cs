96419df4dfedfa29ddbf479420bb1f6d88b158f5
José Mira <jose.mira@paymentsense.com>
2014-06-17 15:39:00 +0100

    Moved SessionManager to Core since it is being used in Services, Web and Dialler

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/ISessionManager.cs
@@ -0,0 +1,9 @@
+﻿using PS.Mothership.Core.Common.Dto.Session;
+
+namespace PS.Mothership.Core.Common.Contracts
+{
+    public interface ISessionManager
+    {
+        SessionHeaderDto SessionInformation { get; set; }
+    }
+}
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Helper/SessionManager.cs
@@ -0,0 +1,16 @@
+﻿using PS.Mothership.Core.Common.Contracts;
+using PS.Mothership.Core.Common.Dto.Session;
+
+namespace PS.Mothership.Core.Common.Helper
+{
+    public class SessionManager : ISessionManager
+    {
+        public SessionHeaderDto SessionInformation { get; set; }
+
+
+        public SessionManager()
+        {
+            SessionInformation = new SessionHeaderDto();
+        }
+    }
+}
