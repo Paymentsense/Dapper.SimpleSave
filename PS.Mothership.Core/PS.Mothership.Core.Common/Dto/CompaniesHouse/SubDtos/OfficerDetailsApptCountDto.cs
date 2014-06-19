f14177dfca1af91aa0115e844b62e0aec3b1538f
Guy Butler <guy.butler@paymentsense.com>
2014-06-19 15:22:42 +0100

    Squashed 'Externals/Core/' changes from 94f6cf6..b93f00e
    b93f00e missed a name
    
    git-subtree-dir: Externals/Core
    git-subtree-split: b93f00eaaaad8535a771d8786c5e2f4b9c7dcc89

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/CompaniesHouse/SubDtos/OfficerDetailsApptCountDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/CompaniesHouse/SubDtos/OfficerDetailsApptCountDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace PS.Mothership.Core.Common.Dto.CompaniesHouse.SubDtos
 {
-    public class OfficerDetailsApptCount
+    public class OfficerDetailsApptCountDto
     {
         public string NumCurrentAppt { get; set; }
         public string NumDissolvedAppt { get; set; }
