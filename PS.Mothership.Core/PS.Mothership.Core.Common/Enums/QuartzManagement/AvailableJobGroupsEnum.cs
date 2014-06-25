21ba96f12f11719d0aca393770df65bc34066eb8
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-06-25 11:15:33 +0100

    QuartzManagement enums and Imanageable interface added to core

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Enums/QuartzManagement/AvailableJobGroupsEnum.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace PS.Mothership.Core.Common.Enums.QuartzManagement
+{
+    [DataContract]
+    public enum AvailableJobGroupsEnum
+    {
+        [EnumMember]
+        Default = 0,
+        [EnumMember]
+        JobGroup1 = 1,
+        [EnumMember]
+        JobGroup2 = 2,
+        [EnumMember]
+        JobGroup3 = 3,
+        [EnumMember]
+        JobGroup4 = 4,
+        [EnumMember]
+        JobGroup5 = 5
+    }
+}
