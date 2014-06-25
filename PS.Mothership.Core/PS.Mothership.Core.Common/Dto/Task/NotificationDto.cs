269e0164d925685839296cc52102c6fe3f5d1c21
Guy Butler <guy.butler@paymentsense.com>
2014-06-25 17:23:09 +0100

    added notification dto

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Task/NotificationDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Template.Event;
+
+namespace PS.Mothership.Core.Common.Dto.Task
+{
+    [DataContract]
+    public class NotificationDto
+    {
+        [DataMember]
+        public Guid NotificationGuid { get; set; }
+        [DataMember]
+        public Guid EventGuid { get; set; }
+        [DataMember]
+        public Guid UpdatedSessionGuid { get; set; }
+        [DataMember]
+        public EventNotificationMethodEnum NotificationMethodKey { get; set; }
+        [DataMember]
+        public DateTime ScheduledDate { get; set; }
+        [DataMember]
+        public DateTime UpdateDate { get; set; }
+    }
+}
