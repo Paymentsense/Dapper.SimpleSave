df8ced60bd218cef98cc9b6266a3a86d8ba14a45
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-06-25 14:35:42 +0100

    Squashed 'Externals/Core/' changes from bf117eb..9a8082e
    9a8082e Added IQuartzManagementService and required DTOs
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 9a8082ead7941cd8e52f3bcc2cd553fad36bcdbd

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/QuartzManagement/JobGroupStateDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace PS.Mothership.Core.Common.Dto.QuartzManagement
+{
+    public class JobGroupStateDto
+    {
+        public string Name { get; set; }
+        public bool Paused { get; set; }
+    }
+}
