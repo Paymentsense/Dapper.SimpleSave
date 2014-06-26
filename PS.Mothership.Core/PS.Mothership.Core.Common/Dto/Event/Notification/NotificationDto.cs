d4decd9f22893f33691041fba5c6e3cc5ec3c671
Guy Butler <guy.butler@paymentsense.com>
2014-06-26 10:39:28 +0100

    Organising Dtos for notifications

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Event/Notification/NotificationDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Dto.Event.Notification;
+
+namespace PS.Mothership.Core.Common.Dto.Event
+{
+    [DataContract]
+    public class NotificationDto
+    {
+       
+        [DataMember]
+        public DateTime TaskStartDateTime { get; set; }
+        [DataMember]
+        public string UserName { get; set; }
+        [DataMember]
+        public Guid UserGuid { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public bool IsRecurring { get; set; }
+        [DataMember]
+        public int Priority { get; set; }
+        [DataMember]
+        public List<PendingTaskNotificationDto> PendingTaskNotifications { get; set; }
+    }
+}
