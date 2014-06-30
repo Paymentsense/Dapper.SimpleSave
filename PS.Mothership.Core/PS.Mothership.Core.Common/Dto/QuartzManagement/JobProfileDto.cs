79b8a3c14ea7eb79902af1822cfbe8ba358d9e22
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-06-30 16:09:53 +0100

    Squashed 'Externals/Core/' changes from c0d6199..f6a1559
    f6a1559 contract update
    a4fbc03 Merge branch 'master' into QuartzManagementUI
    fd0cccf Quartz Server Management changes
    7e86aeb Merge pull request #14 in MS2/mothershipv2_core from UserFixes to master
    731e414 Merge branch 'master' into QuartzManagementUI
    1d1357b commit before merge
    dd1873b Quartz Server Management corrections
    9a8082e Added IQuartzManagementService and required DTOs
    bf117eb QuartzManagement enums and Imanageable interface added to core
    
    git-subtree-dir: Externals/Core
    git-subtree-split: f6a155907f6367218df113fe3b8bb42923eb0cb2

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/QuartzManagement/JobProfileDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using PS.Mothership.Core.Common.Enums.QuartzManagement;
+
+namespace PS.Mothership.Core.Common.Dto.QuartzManagement
+{
+    public class JobProfileDto
+    {
+        public string Name { get; set; }
+        public AvailableJobGroupsEnum Group { get; set; }
+        public IList<TriggerProfileDto> Triggers { get; set; }
+    }
+}
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/QuartzManagement/QuartzServerInfoDto.cs
@@ -0,0 +1,14 @@
+﻿using PS.Mothership.Core.Common.Enums.QuartzManagement;
+using System;
+
+namespace PS.Mothership.Core.Common.Dto.QuartzManagement
+{
+    public class QuartzServerInfoDto
+    {
+        public string SchedulerName { get; set; }
+        public Type JobStoreType { get; set; }
+        public int NumberOfJobsExecuted { get; set; }
+        public DateTimeOffset? RunningSince { get; set; }
+        public SchedulerStatusEnum SchedulerStatus { get; set; }
+    }
+}
