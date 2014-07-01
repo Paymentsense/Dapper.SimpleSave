8bcd90346ffe207061af90bd279c91425b3388a2
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-07-01 17:33:54 +0100

    Squashed 'Externals/Core/' changes from 36b5d64..2a8c277
    2a8c277 Merge branch 'master' into QuartzManagementUI
    19f4d37 Quartz Server Management changes
    f6a1559 contract update
    a4fbc03 Merge branch 'master' into QuartzManagementUI
    fd0cccf Quartz Server Management changes
    731e414 Merge branch 'master' into QuartzManagementUI
    1d1357b commit before merge
    dd1873b Quartz Server Management corrections
    9a8082e Added IQuartzManagementService and required DTOs
    bf117eb QuartzManagement enums and Imanageable interface added to core
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 2a8c27706b3ebbc98c3f0f28bae6b10606708a84

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IQuartzManagementService.cs
@@ -0,0 +1,46 @@
+﻿using PS.Mothership.Core.Common.Constructs;
+using PS.Mothership.Core.Common.Dto.DynamicRequest;
+using PS.Mothership.Core.Common.Dto.QuartzManagement;
+using PS.Mothership.Core.Common.Enums.QuartzManagement;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace PS.Mothership.Core.Common.Contracts
+{
+    [ServiceContract(Name = "QuartzManagementService")]
+    public interface IQuartzManagementService
+    {
+        [OperationContract]
+        QuartzServerInfoDto GetQuartzServerInfo();
+
+        [OperationContract]
+        void ScheduleNewJob(string triggerName, string jobName, AvailableJobGroupsEnum jobGroupName,
+            string jobClass, IntervalUnit occurance, string interval, DateTimeOffset startTime, IEnumerable<DayOfWeek> dayOfWeek = null);
+
+        [OperationContract]
+        void PauseAllTriggers();
+
+        [OperationContract]
+        void ResumeAllTriggers();
+
+        [OperationContract]
+        void PauseJob(string jobName);
+
+        [OperationContract]
+        void ResumeJob(string jobName);
+
+        [OperationContract]
+        void PauseTrigger(string triggerName);
+
+        [OperationContract]
+        void ResumeTrigger(string triggerName);
+
+        [OperationContract]
+        PagedList<JobProfileDto> GetJobs(DataRequestDto dataRequestDto);
+
+        [OperationContract]
+        void ExecuteJobNow(string jobName);
+    }
+}
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/QuartzManagement/TriggerProfileDto.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+using System;
+
+namespace PS.Mothership.Core.Common.Dto.QuartzManagement
+{
+    public class TriggerProfileDto
+    {
+        public string Name { get; set; }
+        public TriggerState State { get; set; }
+        public DateTimeOffset? NextFireTime { get; set; }
+        public string Schedule { get; set; }
+    }
+}
