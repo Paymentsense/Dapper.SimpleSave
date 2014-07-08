428f8c3418ed1b05edf31e5f604ebc68e4e028bf
gregg.kurywchak <gregg.kurywchak@ksfii.com>
2014-07-08 13:06:43 -0400

    Added Status Code to Dto

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Iridium/IridiumConfirmResultsDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Iridium/IridiumConfirmResultsDto.cs
@@ -5,6 +5,7 @@ namespace PS.Mothership.Core.Common.Dto.Iridium
     {
         public bool WasAccountConfirmed { get; set; }
         public string ResultString { get; set; }
+        public int StatusCode { get; set; }
     }
 
 }
