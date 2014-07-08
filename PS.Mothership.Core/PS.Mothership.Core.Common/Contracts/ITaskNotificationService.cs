f9bdab725cfcbe4b088a74d53dda2000fec9f582
Guy Butler <guy.butler@paymentsense.com>
2014-07-08 11:36:46 +0100

    undo last comit

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/ITaskNotificationService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/ITaskNotificationService.cs
@@ -8,6 +8,10 @@ namespace PS.Mothership.Core.Common.Contracts
     [ServiceContract(CallbackContract = typeof(ITaskNotificationCallback))]
     public interface ITaskNotificationService : IQuartzJobBase
     {
+        [OperationContract(IsOneWay = false)]
+        void Subscribe(string applicationName);
+        [OperationContract(IsOneWay = true)]
+        void EndSubscribe(string applicationName);
         [OperationContract]
         TaskNotificationDto AddNewTask(TaskNotificationDto taskNotificationDto);
         [OperationContract]
