329ff6380313f881a61274fd161e293d4a6707d3
Guy Butler <guy.butler@paymentsense.com>
2014-07-08 11:28:15 +0100

    Added INotification interface

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/INotificationService.cs
@@ -0,0 +1,9 @@
+﻿namespace PS.Mothership.Core.Common.Contracts
+{
+    interface INotificationService
+    {
+        void Subscribe(string applicationName);
+
+        void EndSubscribe(string applicationName);
+    }
+}
