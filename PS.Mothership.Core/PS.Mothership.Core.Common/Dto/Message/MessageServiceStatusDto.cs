c5099a40b98d08e8f9e8d78327ff604ac478affc
Guy Butler <guy.butler@paymentsense.com>
2014-07-09 15:29:56 +0100

    message service status dto updated

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Message/MessageServiceStatusDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Message/MessageServiceStatusDto.cs
@@ -1,6 +1,10 @@
-﻿namespace PS.Mothership.Core.Common.Dto.Message
+﻿using PS.Mothership.Core.Common.Template.Comm;
+
+namespace PS.Mothership.Core.Common.Dto.Message
 {
     public class MessageServiceStatusDto
     {
+        public CommMessageServiceStatusEnum MessageServiceStatus { get; set; }
+        public CommMessageTypeEnum MessageTypeEnum { get; set; }
     }
 }
