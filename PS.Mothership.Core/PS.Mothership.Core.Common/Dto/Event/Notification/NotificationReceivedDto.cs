5f6402530a662fb73f27d28838df0fe5adb0968c
Guy Butler <guy.butler@paymentsense.com>
2014-07-11 09:50:28 +0100

    Squashed 'Externals/Core/' changes from d68e6f3..b1d0255
    b1d0255 Message notification push callback
    
    git-subtree-dir: Externals/Core
    git-subtree-split: b1d025565995f2dfd66aa5a4ed7a4ff4f0dd08b1

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Event/Notification/NotificationReceivedDto.cs
@@ -0,0 +1,10 @@
+﻿using PS.Mothership.Core.Common.Template.Event;
+
+namespace PS.Mothership.Core.Common.Dto.Event.Notification
+{
+    public class NotificationReceivedDto
+    {
+        EventTypeEnum EventType { get; set; }
+        int NotificationAmount { get; set; }
+    }
+}
