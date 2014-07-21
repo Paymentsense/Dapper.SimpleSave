a7dae534723077bf11954a7482c7fea1bd12a277
José Mira <jose.mira@paymentsense.com>
2014-07-21 17:23:50 +0100

    Sync of Core with Externals from Services and Web

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IProspectCustomerService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IProspectCustomerService.cs
@@ -43,5 +43,12 @@ namespace PS.Mothership.Core.Common.Contracts
 
         [OperationContract]
         PagedList<ProspectAddressDto> GetSimilarMerchants(FullAddressDto dto);
+
+        [OperationContract]
+        PagedList<ProspectAddressDto> QuickSearch(SearchDto dto);
+
+        [OperationContract]
+        PagedList<MerchantAdvancedSearchDto> AdvancedSearch(DataRequestDto dto);
+
     }
 }
