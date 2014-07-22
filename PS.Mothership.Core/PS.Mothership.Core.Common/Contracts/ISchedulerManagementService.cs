86abda02b3f848f1c57d65600d29e65d85edf3b6
José Mira <jose.mira@paymentsense.com>
2014-07-22 09:33:47 +0100

    Squashed 'Externals/Core/' changes from 2d92acd..9d0f622
    9d0f622 Merge branch 'master' into QuartzManagementUI
    e157f04 Merge pull request #16 in MS2/mothershipv2_core from MerchantsSearch to master
    b7cc464 Added GetTriggers and GetTrigger to Scheduler Management Contract
    4db9fcf update made to contract
    35ead28 update made to contract
    267d9c2 Added Active property to JobProfileDto
    275a952 Merge branch 'master' into QuartzManagementUI
    b34facf Schduler refactoring and move Scheduler Job Group enumeration to database
    91712bf refactor DeleteTriggerFromJob to DeleteTrigger
    99f810e update Quartz Management Contract
    2a8c277 Merge branch 'master' into QuartzManagementUI
    19f4d37 Quartz Server Management changes
    f6a1559 contract update
    a4fbc03 Merge branch 'master' into QuartzManagementUI
    fd0cccf Quartz Server Management changes
    731e414 Merge branch 'master' into QuartzManagementUI
    1d1357b commit before merge
    dd1873b Quartz Server Management corrections
    9a8082e Added IQuartzManagementService and required DTOs
    bf117eb QuartzManagement enums and Imanageable interface added to core
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 9d0f6222cc67293b754fdce22aa58990e2d81738

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/ISchedulerManagementService.cs
@@ -0,0 +1,66 @@
+﻿using PS.Mothership.Core.Common.Constructs;
+using PS.Mothership.Core.Common.Dto.DynamicRequest;
+using PS.Mothership.Core.Common.Dto.SchedulerManagement;
+using PS.Mothership.Core.Common.Template.Gen;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace PS.Mothership.Core.Common.Contracts
+{
+    [ServiceContract(Name = "SchedulerManagementService")]
+    public interface ISchedulerManagementService
+    {
+        [OperationContract]
+        SchedulerServerInfoDto GetSchedulerServerInfo();
+
+        [OperationContract]
+        void ScheduleNewJob(string triggerName, string jobName, GenSchedulerJobGroupEnum jobGroupName,
+            string jobClass, IntervalUnit occurance, string interval, DateTimeOffset startTime, IEnumerable<DayOfWeek> dayOfWeek = null);
+
+        [OperationContract]
+        void PauseAllTriggers();
+
+        [OperationContract]
+        void ResumeAllTriggers();
+
+        [OperationContract]
+        void PauseJob(string jobName);
+
+        [OperationContract]
+        void ResumeJob(string jobName);
+
+        [OperationContract]
+        void PauseTrigger(string triggerName);
+
+        [OperationContract]
+        void ResumeTrigger(string triggerName);
+
+        [OperationContract]
+        PagedList<JobProfileDto> GetJobs(DataRequestDto dataRequestDto);
+        
+        [OperationContract]
+        JobProfileDto GetJob(string jobName);
+
+        [OperationContract]
+        PagedList<TriggerProfileDto> GetTriggers(string jobName, DataRequestDto dataRequestDto);
+
+        [OperationContract]
+        TriggerProfileDto GetTrigger(string jobName, string triggerName);
+
+        [OperationContract]
+        void ExecuteJobNow(string jobName);
+
+        [OperationContract]
+        void AddTriggerToJob(string triggerName, string jobName,
+            IntervalUnit occurance, string interval, DateTimeOffset startTime, IEnumerable<DayOfWeek> dayOfWeek = null);
+
+        [OperationContract]
+        void DeleteTrigger(string triggerName);
+
+        [OperationContract]
+        void RescheduleTrigger(string triggerName, IntervalUnit occurance, string interval, DateTimeOffset startTime,
+            IEnumerable<DayOfWeek> dayOfWeek = null);
+    }
+}
