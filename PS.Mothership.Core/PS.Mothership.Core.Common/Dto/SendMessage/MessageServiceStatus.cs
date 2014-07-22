83e88a8cb249b498ec19ae140bd4dad3c5aaaaa5
Guy Butler <guy.butler@paymentsense.com>
2014-07-22 14:56:01 +0100

    improving the service caching

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/SendMessage/MessageServiceStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Template.Comm;
+
+namespace PS.Mothership.Core.Common.Dto.SendMessage
+{
+    [DataContract]
+    public class MessageServiceStatus
+    {
+        [DataMember]
+        public CommMessageServiceStatusEnum MessageStatus { get; set; }
+        [DataMember]
+        public CommMessageTypeEnum MessageType { get; set; }
+        [DataMember]
+        public DateTimeOffset Time { get; set; }
+    }
+}
--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/SendMessage/SendMessageServiceStatusDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/SendMessage/SendMessageServiceStatusDto.cs
@@ -1,16 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
-using PS.Mothership.Core.Common.Template.Comm;
 
 namespace PS.Mothership.Core.Common.Dto.SendMessage
 {
 	[DataContract]
 	public class SendMessageServiceStatusDto
 	{
-		[DataMember]
-		public CommMessageServiceStatusEnum MessageStatus { get; set; }
-		[DataMember]
-		public CommMessageTypeEnum MessageType { get; set; }
+        [DataMember]
+        public IList<MessageServiceStatus> MessageServiceStatus { get; set; }
 		[DataMember]
 		public DateTimeOffset LastUpdateTime { get; set; }
 	}
