184246a24d7d96be1c7c8cba8734109b1c79b20b
Guy Butler <guy.butler@paymentsense.com>
2014-07-23 17:04:19 +0100

    Squashed 'Externals/Core/' changes from 3bd3188..9bff002
    9bff002 add InboundMessage to core
    15ea0ac Merge branch 'master' into TaskNotification
    e157f04 Merge pull request #16 in MS2/mothershipv2_core from MerchantsSearch to master
    2d92acd Merge branch 'MerchantsAdvancedSearch' into MerchantsSearch
    b90f3cc Sync of Core with Externals from Services and Web
    0eac885 Sync of Core with Externals from Services and Web
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 9bff0026542b236c074651d047a783b99c7c5fc7

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Message/InboundMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PS.Mothership.Core.Common.Dto.Message
+{
+    public class InboundMessage
+    {
+        public Guid Id { get; set; }
+        public Guid MessageId { get; set; }
+        public Guid AccountId { get; set; }
+        public string Subject { get; set; }
+        public string MessageText { get; set; }
+        public string From { get; set; }
+        public IList<string> To { get; set; }
+        public DateTime Now { get; set; }
+    }
+}
