accda4e3f4d465ab457a1f6d6f5d7dbf4ed11642
José Mira <jose.mira@paymentsense.com>
2014-07-25 17:11:59 +0100

    Squashed 'Externals/Core/' changes from bc69121..f477acd
    f477acd Added extra fields to TriggerProfileDto
    
    git-subtree-dir: Externals/Core
    git-subtree-split: f477acd579e30644747fe662cc1805d1eb6d5fe4

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/SchedulerManagement/TriggerProfileDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/SchedulerManagement/TriggerProfileDto.cs
@@ -1,5 +1,7 @@
-﻿using Quartz;
+﻿using PS.Mothership.Core.Common.Template.Gen;
+using Quartz;
 using System;
+using System.Collections.Generic;
 
 namespace PS.Mothership.Core.Common.Dto.SchedulerManagement
 {
@@ -9,5 +11,10 @@ namespace PS.Mothership.Core.Common.Dto.SchedulerManagement
         public TriggerState State { get; set; }
         public DateTimeOffset? NextFireTime { get; set; }
         public string Schedule { get; set; }
+        public DateTimeOffset StartTime { get; set; }
+        public IntervalUnit Occurrence { get; set; }
+        public int TriggerInterval { get; set; }
+        public IEnumerable<DayOfWeek> SelectedDaysOfWeek { get; set; }
+        public GenSchedulerJobGroupEnum JobGroup { get; set; }
     }
 }
