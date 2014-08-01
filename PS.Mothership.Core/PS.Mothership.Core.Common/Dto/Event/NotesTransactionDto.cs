38aa507708aa0086499168cd970125209da194f4
Guy Butler <guy.butler@paymentsense.com>
2014-08-01 15:42:39 +0100

    updated dto structure to mirror that of what is in the entity framework

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Event/NotesTransactionDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Event/NotesTransactionDto.cs
@@ -10,10 +10,18 @@ namespace PS.Mothership.Core.Common.Dto.Event
         [DataMember]
         public Guid NotesGuid { get; set; }
         [DataMember]
+        public Guid EventGuid { get; set; }
+        [DataMember]
         public string Notes { get; set; }
         [DataMember]
         public EventNotesTypeEnum NotesTypeKey { get; set; }
         [DataMember]
-        public Guid EventGuid { get; set; }
+        public DateTimeOffset UpdateDate { get; set; }
+        [DataMember]
+        public Guid UpdateSessionGuid { get; set; }
+        [DataMember]
+        public EventTransactionDto Event { get; set; }
+        [DataMember]
+        public EventNotesLnkDto EventNoteLnk { get; set; }
     }
 }
--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Event/StatusOwnerTransactionDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Event/StatusOwnerTransactionDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 using PS.Mothership.Core.Common.Template.Event;
 
@@ -10,12 +11,16 @@ namespace PS.Mothership.Core.Common.Dto.Event
         [DataMember]
         public Guid StatusOwnerGuid { get; set; }
         [DataMember]
+        public Guid EventGuid { get; set; }
+        [DataMember]
         public EventStatusEnum EventStatusKey { get; set; }
         [DataMember]
         public Guid UserGuid { get; set; }
         [DataMember]
-        public Guid EventGuid { get; set; }
-        [DataMember]
         public DateTimeOffset UpdateDate { get; set; }
+        [DataMember]
+        public EventTransactionDto Event { get; set; }
+        [DataMember]
+        public IList<EventTransactionDto> EventIsCurrent { get; set; }
     }
 }
