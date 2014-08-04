4250e6ae67ac4f7546201da34c8b11ee03d5008a
Alpesh Mistry <alpesh.mistry@paymentsense.com>
2014-08-04 17:30:44 +0100

    Validation regular expressions added to postcode, phone number, mobile number, create from/to, and last update from/to.

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/AdvancedSearchDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/AdvancedSearchDto.cs
@@ -1,46 +1,22 @@
 ﻿using System.Runtime.Serialization;
 
+
 namespace PS.Mothership.Core.Common.Dto.Merchant
 {
     [DataContract]
     public class AdvancedSearchDto
     {
         [DataMember]
+        public string BankAccount { get; set; }
+
+        [DataMember]
+        public string BankMid { get; set; }
+
+        [DataMember]
         public string BusinessName { get; set; }
 
         [DataMember]
-        public string FirstName { get; set; }
-
-        [DataMember]
-        public string Surname { get; set; }
-
-        [DataMember]
-        public string EmailAddress { get; set; }
-
-        [DataMember]
-        public string Street { get; set; }
-
-        [DataMember]
-        public string Town { get; set; }
-
-        [DataMember]
-        public string PostCode { get; set; }
-
-
-        [DataMember]
-        public string PhoneNumber { get; set; }
-
-        [DataMember]
-        public string Mobile { get; set; }
-
-        [DataMember]
-        public long V1MerchantId { get; set; }
-
-        [DataMember]
-        public string LocatorId { get; set; }
-
-        [DataMember]
-        public string RegisteredNo { get; set; }
+        public string BusinessType { get; set; }
 
         [DataMember]
         public string CreatedFrom { get; set; }
@@ -49,43 +25,68 @@ namespace PS.Mothership.Core.Common.Dto.Merchant
         public string CreatedTo { get; set; }
 
         [DataMember]
+        public string Department { get; set; }
+
+        [DataMember]
+        public string EmailAddress { get; set; }
+
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
         public string LastUpdateFrom { get; set; }
 
         [DataMember]
         public string LastUpdateTo { get; set; }
 
         [DataMember]
-        public string Stage { get; set; }
+        public string LocatorId { get; set; }
 
         [DataMember]
-        public string SubStage { get; set; }
-
-        [DataMember]
-        public string Department { get; set; }
-
-        [DataMember]
-        public string Owner { get; set; }
-
-        [DataMember]
-        public string Source { get; set; }
-
-        [DataMember]
-        public string BusinessType { get; set; }
-
-        [DataMember]
-        public long ThompsonCode { get; set; }
-
-        [DataMember]
-        public string Provisioner { get; set; }
+        public string Mobile { get; set; }
 
         [DataMember]
         public string OpportunityId { get; set; }
 
         [DataMember]
-        public string BankMid { get; set; }
+        public string Owner { get; set; }
 
         [DataMember]
-        public string BankAccount { get; set; }
+        
+        public string PhoneNumber { get; set; }
+
+        [DataMember]
+        public string PostCode { get; set; }
+
+        [DataMember]
+        public string Provisioner { get; set; }
+
+        [DataMember]
+        public string RegisteredNo { get; set; }
+
+        [DataMember]
+        public string Source { get; set; }
+
+        [DataMember]
+        public string Stage { get; set; }
+
+        [DataMember]
+        public string Street { get; set; }
+
+        [DataMember]
+        public string SubStage { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public string ThompsonCode { get; set; }
+          
+        [DataMember]
+        public string Town { get; set; }
+
+        [DataMember]
+        public string V1MerchantId { get; set; }
 
     }
 }
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Validation/GeneralRegex.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.RegularExpressions;
+using PS.Mothership.Core.Common.Constructs;
+
+namespace PS.Mothership.Core.Common.Validation
+{
+    public static class GeneralRegex
+    {
+
+        public static RegexValidation AlphaOnly
+        {
+            get { return new RegexValidation {Regex = "", ErrorMessage = ""}; }
+        }
+
+        public static RegexValidation NumericOnly
+        {
+
+            get { return new RegexValidation {Regex = "", ErrorMessage = ""}; }
+        }
+
+        public static RegexValidation UkDate {
+
+            get
+            {
+                return new RegexValidation
+                {
+                    Regex = @"^(((0[1-9]|[12][0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$", 
+                    ErrorMessage = "Please specify a valid date"
+                };
+            }
+        }
+
+        public static RegexValidation PhoneNumber
+        {
+
+            get
+            {
+                return new RegexValidation
+                {
+                    Regex =@"^(([-\s()]*(\+|00)?[-\s()]*353([-\s()]*\d){9,10}[-\s()]*)|([-\s()]*0?[-\s()]*[1-689]([-\s()]*\d){8,9}[-\s()]*))$", 
+                    ErrorMessage = "Please specify a valid landline number"
+                };              
+            }
+        }
+
+
+        public static RegexValidation  MobilePhoneNumber 
+        {
+
+            get
+            {
+                return new RegexValidation
+                {
+                    Regex = @"^(([-\s()]*(\+|00)?[-\s()]*353([-\s()]*\d){9,10}[-\s()]*)|([-\s()]*0?[-\s()]*7([-\s()]*\d){8,9}[-\s()]*))$",
+                    ErrorMessage = "Please specify a mobile number"
+                };
+
+            }
+        }
+
+        public static RegexValidation EmailAddress
+        {
+
+            get
+            {
+                return new RegexValidation
+                {
+                    Regex = @"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$",
+                    ErrorMessage = "Please specify a valid email address"
+                };
+
+            }
+        }
+
+    }
+}
