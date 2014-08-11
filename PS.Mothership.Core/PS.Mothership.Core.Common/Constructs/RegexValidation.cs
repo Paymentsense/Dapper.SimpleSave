275eb49ad4e06dea336665e5c54bdf5e43071db9
Alpesh Mistry <alpesh.mistry@paymentsense.com>
2014-08-11 09:42:13 +0100

    Squashed 'Externals/Core/' changes from a5d57c4..fdc7f8f
    fdc7f8f Merge branch 'master' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into MerchantAdvancedSearchParams
    423fb15 New Text search and advanced search DTOs, for search merchants processing. New Regex validation. ApplicationService behaviour set for concurrencyMode.Multiple.
    c53dc83 Updated Validation Regex to include Business name and PersonName, and AlphaNumericOnly expressions.
    766a105 Validation regular expressions added to postcode, phone number, mobile number, create from/to, and last update from/to.
    46467d0 Merge branch 'master' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into MerchantAdvancedSearchParams
    1d97bcb Added Stage parameters to search merchants on status.
    a27adb6 Sync core with externals
    
    git-subtree-dir: Externals/Core
    git-subtree-split: fdc7f8fdf09bd9dd4ce2635ce2eef9cbf08ac716

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Constructs/RegexValidation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Constructs
+{
+    public class RegexValidation
+    {
+        public string Regex { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
