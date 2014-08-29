7445f2d13a2ef6132649a3db94deed63062eeec6
Alpesh Mistry <alpesh.mistry@paymentsense.com>
2014-08-29 10:42:09 +0100

    Squashed 'Externals/Core/' changes from 051a013..16a36b7
    16a36b7 Includes ApplicationService.cs
    cd2c014 Merge branch 'master' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into MerchantAdvancedSearchParams
    9c6ad25 Merge pull request #20 in MS2/mothershipv2_core from RepositoryRefactoringTests to master
    f781c5d Merge pull request #22 in MS2/mothershipv2_core from AnnualTurnoverTextBox to master
    c2c5f3b Includes MerchantSearchDataDto.
    6973c7a Includes RegexValidation class.
    a33d39d Merge branch 'master' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into MerchantAdvancedSearchParams
    7cb9ed5 Merge branch 'master' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into AnnualTurnoverTextBox
    bfc8c17 Annual turnover longs.
    39875c6 Removed full stops.
    7c7a63e QuickSearch functionality.
    fdc7f8f Merge branch 'master' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into MerchantAdvancedSearchParams
    423fb15 New Text search and advanced search DTOs, for search merchants processing. New Regex validation. ApplicationService behaviour set for concurrencyMode.Multiple.
    c53dc83 Updated Validation Regex to include Business name and PersonName, and AlphaNumericOnly expressions.
    766a105 Validation regular expressions added to postcode, phone number, mobile number, create from/to, and last update from/to.
    46467d0 Merge branch 'master' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into MerchantAdvancedSearchParams
    1d97bcb Added Stage parameters to search merchants on status.
    a27adb6 Sync core with externals
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 16a36b7bd90d7d11da2322d5947bc392d1cf94b6

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/AdvancedSearchDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/AdvancedSearchDto.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Template.Cust;
+
 
 namespace PS.Mothership.Core.Common.Dto.Merchant
 {
@@ -6,24 +8,86 @@ namespace PS.Mothership.Core.Common.Dto.Merchant
     public class AdvancedSearchDto
     {
         [DataMember]
+        public string BankAccount { get; set; }
+
+        [DataMember]
+        public string BankMid { get; set; }
+
+        [DataMember]
         public string BusinessName { get; set; }
 
         [DataMember]
-        public string FirstName { get; set; }
+        public string BusinessType { get; set; }
 
         [DataMember]
-        public string Surname { get; set; }
+        public string CreatedFrom { get; set; }
+
+        [DataMember]
+        public string CreatedTo { get; set; }
+
+        [DataMember]
+        public string Department { get; set; }
 
         [DataMember]
         public string EmailAddress { get; set; }
 
         [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastUpdateFrom { get; set; }
+
+        [DataMember]
+        public string LastUpdateTo { get; set; }
+
+        [DataMember]
+        public string LocatorId { get; set; }
+
+        [DataMember]
+        public string Mobile { get; set; }
+
+        [DataMember]
+        public string OpportunityId { get; set; }
+
+        [DataMember]
+        public string Owner { get; set; }
+
+        [DataMember]    
+        public string PhoneNumber { get; set; }
+
+        [DataMember]
+        public string PostCode { get; set; }
+
+        [DataMember]
+        public string Provisioner { get; set; }
+
+        [DataMember]
+        public string RegisteredNo { get; set; }
+
+        [DataMember]
+        public string Source { get; set; }
+
+        [DataMember]
+        public string Stage { get; set; }
+
+        [DataMember]
         public string Street { get; set; }
 
+        [DataMember]
+        public string SubStage { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public string ThompsonCode { get; set; }
+          
         [DataMember]
         public string Town { get; set; }
 
         [DataMember]
-        public string PostCode { get; set; }
+        public string V1MerchantId { get; set; }
+
+
     }
 }
