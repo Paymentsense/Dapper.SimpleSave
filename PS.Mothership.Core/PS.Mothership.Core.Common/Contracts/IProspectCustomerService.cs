dcc3eeadedc4f627a0d1521cfe290b97d7271553
David Welch <david.welch@paymentsense.com>
2014-09-02 16:19:31 +0100

    Squashed 'Externals/Core/' changes from a358914..194e0dd
    194e0dd Get latest offers for merchant query.
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 194e0dd203402544d7465803ecc8873fcdd93cc0

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IProspectCustomerService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IProspectCustomerService.cs
@@ -3,6 +3,7 @@ using PS.Mothership.Core.Common.Dto;
 using PS.Mothership.Core.Common.Dto.DynamicRequest;
 using PS.Mothership.Core.Common.Dto.Merchant;
 using System;
+using System.Collections.Generic;
 using System.ServiceModel;
 
 namespace PS.Mothership.Core.Common.Contracts
@@ -10,6 +11,9 @@ namespace PS.Mothership.Core.Common.Contracts
     [ServiceContract(Name = "ProspectCustomerService")]
     public interface IProspectCustomerService
     {
+        [OperationContract]
+        IEnumerable<MerchantOfferDto> GetLatestOffersForMerchant(Guid merchantGuid);
+
         [OperationContract]
         ProspectDto GetProspect(Guid merchantGuid);
 
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/MerchantOfferDto.cs
@@ -0,0 +1,30 @@
+﻿using PS.Mothership.Core.Common.Template.Opp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Dto.Merchant
+{
+    public class MerchantOfferDto
+    {
+        public Guid CustomerGuid { get; set; }
+
+        public Guid OfferGuid { get; set; }
+
+        public string Reference { get; set; }
+
+        public OppOpportunityStatusEnum Status { get; set; }
+
+        public Decimal Credit { get; set; }
+
+        public Decimal Debit { get; set; }
+
+        public OppContractLengthEnum Duration { get; set; }
+
+        public DateTimeOffset UpdateDate { get; set; }
+
+        public string UpdateUsername { get; set; }
+    }
+}
