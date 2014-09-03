b583cce1a5a60df26f364f3f03fd73e3e15e1258
José Mira <jose.mira@paymentsense.com>
2014-09-03 14:43:12 +0100

    Squashed 'Externals/Core/' changes from 087e04b..f911709
    f911709 Sync Core across solutions
    
    git-subtree-dir: Externals/Core
    git-subtree-split: f911709e145fbeef1166e88355a7e588ad62b5c7

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/ProspectResponseDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/ProspectResponseDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
-using PS.Mothership.Core.Common.Contracts;
+﻿using System.Runtime.Serialization;
 using PS.Mothership.Core.Common.Enums;
 
 namespace PS.Mothership.Core.Common.Dto.Merchant
