84fff800f66573bce1c408572fe3f6e40e08abe0
José Mira <jose.mira@paymentsense.com>
2014-09-04 14:43:06 +0100

    Added comments Corrected Postcode validation to allow ROI

--- PS.Mothership.Core/PS.Mothership.Core.Common/Validation/AddressRegex.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Validation/AddressRegex.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PS.Mothership.Core.Common.Constructs;
+﻿using PS.Mothership.Core.Common.Constructs;
 
 namespace PS.Mothership.Core.Common.Validation
 {
     public static class AddressRegex
     {
 
-
+        /// <summary>
+        /// Regex checks UK postcodes plus ROI (to currently handle RoI addresses)
+        /// </summary>
         public static RegexValidation PostCode
         {
             get
@@ -18,7 +15,7 @@ namespace PS.Mothership.Core.Common.Validation
                 return new RegexValidation()
                 {
 
-                    Regex = @"^([Gg][Ii][Rr] 0[Aa][Aa]|[A-PR-UWYZa-pr-uwyz]([0-9][0-9A-HJKPS-UWa-hjkps-uw]?|[A-HK-Ya-hk-y][0-9][0-9ABEHMNPRV-Yabehmnprv-y]?)\s?[0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2})$",
+                    Regex = @"^(([Rr][Oo][Ii])|([Gg][Ii][Rr] 0[Aa][Aa]|[A-PR-UWYZa-pr-uwyz]([0-9][0-9A-HJKPS-UWa-hjkps-uw]?|[A-HK-Ya-hk-y][0-9][0-9ABEHMNPRV-Yabehmnprv-y]?)\s?[0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}))$",
                     ErrorMessage = "Please specify a valid postcode"
                 };
             }
