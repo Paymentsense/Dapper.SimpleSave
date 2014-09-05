76255e4b9d2d03403813df5ac7900b439703a0c8
Alpesh Mistry <alpesh.mistry@paymentsense.com>
2014-09-05 15:43:09 +0100

    update

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/OfferMetaDataDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/OfferMetaDataDto.cs
@@ -7,11 +7,20 @@ namespace PS.Mothership.Core.Common.Dto.Merchant
     public class OfferMetaDataDto
     {
 
-        public ICollection<OppContractLengthEnum> Durations { get; set; }
+        public IEnumerable<OppContractLength> Durations { get; set; }
         public IEnumerable<CountryDto> Countries { get; set; }
         public IEnumerable<OfferSalesChannelDto> SalesChannels { get; set; }
         public IEnumerable<OfferAuthorizationFeeDto> AuthorizationFees { get; set; }
         public IEnumerable<OfferMinimumMonthlyServiceChargeDto> MinimumMonthlyServiceCharges { get; set; }
+        public int DefaultDuration { get; set; }
+        public long DefaultCountryKey { get; set; }
+        public long DefaultSalesChannelKey { get; set; }
+        public decimal DefaultFee { get; set; }
+        public int DefaultFeeKey { get; set; }
+        public int DefaultServiceChargeKey { get; set; }
+
+
 
     }
+    
 }
