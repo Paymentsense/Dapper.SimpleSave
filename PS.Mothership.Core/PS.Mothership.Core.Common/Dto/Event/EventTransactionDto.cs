872a6019c5a277ae069eb2d12910fb5b3ff24ec0
Guy Butler <guy.butler@paymentsense.com>
2014-10-02 12:32:15 +0100

    Squashed 'Externals/Core/' changes from 3cedd3c..7dd726f
    git-subtree-dir: Externals/Core
    git-subtree-split: 7dd726f3c89e54c2b68609e59f9cda6461e0cde1

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Event/EventTransactionDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Event/EventTransactionDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Template.Event;
 
 namespace PS.Mothership.Core.Common.Dto.Event
 {
@@ -16,5 +18,23 @@ namespace PS.Mothership.Core.Common.Dto.Event
         public Guid StatusOwnerGuid { get; set; }
         [DataMember]
         public Guid NotificationGuid { get; set; }
+        [DataMember]
+        public DateTimeOffset UpdateDate { get; set; }
+        [DataMember]
+        public Guid UpdateSessionGuid { get; set; }
+        [DataMember]
+        public StatusOwnerTransactionDto StatusOwnerCurrent { get; set; }
+        [DataMember]
+        public IList<StatusOwnerTransactionDto> StatusOwners { get; set; }
+        [DataMember]
+        public IList<NotesTransactionDto> Notes { get; set; }
+        [DataMember]
+        public NotificationTransactionDto NotificationCurrent { get; set; }
+        [DataMember]
+        public IList<NotificationTransactionDto> Notifications { get; set; }
+        [DataMember]
+        public IList<EventRoleLnkDto> EventRoleLnk { get; set; }
+        [DataMember]
+        public IList<EventNotesLnkDto> EventNotesLnk { get; set; }
     }
 }
