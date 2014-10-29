2c3f75b417992eaf59488bec3e0e1c0c880d8068
Guy Butler <guy.butler@paymentsense.com>
2014-10-29 14:11:31 +0000

    adding metadata dtos and contracts for app

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IApplicationBoardingService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IApplicationBoardingService.cs
@@ -1,7 +1,6 @@
 using System;
 using System.ServiceModel;
 using PS.Mothership.Core.Common.Dto.Application;
-using PS.Mothership.Core.Common.Dto.Merchant;
 
 namespace PS.Mothership.Core.Common.Contracts
 {
@@ -15,10 +14,15 @@ namespace PS.Mothership.Core.Common.Contracts
         ApplicationOpportunitiesDto SaveApplicationOpportunities(ApplicationOpportunitiesDto applicationOpportunitiesDto);
 
         [OperationContract]
+        ApplicationOpportunitiesMetadataDto GetApplicationOpportunitiesMetadata();
+
+        [OperationContract]
         ApplicationDetailsDto GetApplicationDetails(Guid applicationGuid);
 
         [OperationContract]
         ApplicationDetailsDto SaveApplicationDetails(ApplicationDetailsDto applicationDetailsDto);
 
+        [OperationContract]
+        ApplicationOpportunitiesMetadataDto GetApplicationDetailMetadata();
     }
 }
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationOpportunitiesMetadataDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Dto.Merchant;
+
+namespace PS.Mothership.Core.Common.Dto.Application
+{
+    [DataContract]
+    public class ApplicationOpportunitiesMetadataDto
+    {
+        [DataMember]
+        public IEnumerable<OfferSalesChannelDto> SalesChannels { get; set; }
+    }
+}
