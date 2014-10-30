123d07960af45ff04f118fbf85286c85f8ddf38a
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-10-30 15:11:13 +0000

    BusinessType Refactor

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationDetailsMetadataDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Template.Gen;
+
+namespace PS.Mothership.Core.Common.Dto.Application
+{
+    [DataContract]
+    public class ApplicationDetailsMetadataDto
+    {
+        public IList<GenBusinessLegalType> BusinessLegalTypes { get; set; }
+    }
+}
