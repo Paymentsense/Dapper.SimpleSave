ad0da0dde1d83ff3ad4f108f2daf8fda9ecbe5ba
Lewis Ayres-Stephens <lewis.ayres-stephens@paymentsense.com>
2014-10-31 11:28:57 +0000

    Squashed 'Externals/Core/' changes from 8835837..3457da0
    3457da0 Merge branch 'Sprint4' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    eb48014 Merge branch 'master' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into Sprint4
    d98a0b3 add data member
    40e0811 BusinessType Refactor
    4edba19 Update templated enumerations
    d60aa23 Updated LocationProductMstDto
    3e11434 Updated LocationMstDto
    01b0a81 Update NuGet packages
    079deca project file update
    afffe15 adding metadata dtos and contracts for app
    d816201 remove duplicate reference
    638aa35 Merging in core differences.
    2400984 Merge branch 'Sprint4' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    d50552c Conflicts: 	PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/ContactDto.cs
    7279c6a Merge branch 'ApplicationMain' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    dd5273d namespace refactor
    0bf93ae update made to core
    7367b3e removed using
    b10c747 added AppRef and corrected namespaces
    c32c42b Merge pull request #32 in MS2/mothershipv2_core from ApplicationBase to Sprint4
    50fa52a Updated ApplicationDetailsDto
    a7d62c0 Updated ApplicationBoardingService
    85d8e73 Created service Contract and Dtos for Application
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 3457da0bbf2ddb8877bd1b18089034a4baa199fe

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IApplicationService.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IApplicationService.cs
@@ -2,6 +2,9 @@ using System.ServiceModel;
 
 namespace PS.Mothership.Core.Common.Contracts
 {
+    /// <summary>
+    /// This is a marker and not to be confused with IApplicationBoardingService which is Domain specific.
+    /// </summary>
     [ServiceContract]
     public interface IApplicationService
     {
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/LocationProductMstDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PS.Mothership.Core.Common.Dto.Application
+{
+    [DataContract]
+    public class LocationProductMstDto
+    {
+        [DataMember]
+        public Guid LocationProductGuid { get; set; }
+
+        [DataMember]
+        public Guid LocationGuid { get; set; }
+
+        [DataMember]
+        public Guid OpportunityGuid { get; set; }
+
+        [DataMember]
+        public Guid ContactGuid { get; set; }
+
+        [DataMember]
+        public Guid MerchantGuid { get; set; }
+    
+    }
+}
