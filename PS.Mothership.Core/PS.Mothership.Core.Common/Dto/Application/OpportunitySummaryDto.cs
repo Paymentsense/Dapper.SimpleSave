f96a35235ee5a978914a7e82e8989c4cbff232c6
José Mira <jose.mira@paymentsense.com>
2014-11-05 15:22:01 +0000

    Squashed 'Externals/Core/' changes from da9e4e9..218d2ea
    218d2ea Sync Core
    c0694bf Merge branch 'Sprint4' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    99a7203 Merge branch 'AppOpportunities' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    f93ab3b Merge branch 'ApplicationMain' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into AppOpportunities
    1752fe0 Merge branch 'ApplicationMain' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    67804f7 template ProdLessorEnum update
    23fb926 Merge branch 'Sprint4' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    96c8994 Merge branch 'Sprint4' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    524c865 Added LocationReference.
    6b08695 Added Application Summary related DTOs
    3457da0 Merge branch 'Sprint4' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    d98a0b3 add data member
    40e0811 BusinessType Refactor
    d60aa23 Updated LocationProductMstDto
    3e11434 Updated LocationMstDto
    079deca project file update
    afffe15 adding metadata dtos and contracts for app
    d816201 remove duplicate reference
    638aa35 Merging in core differences.
    2400984 Merge branch 'Sprint4' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    7279c6a Merge branch 'ApplicationMain' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into ApplicationMain
    dd5273d namespace refactor
    0bf93ae update made to core
    7367b3e removed using
    b10c747 added AppRef and corrected namespaces
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 218d2ea4391c52f9ab4fb83f57424eccf5595a9d

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/OpportunitySummaryDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Dto.Merchant;
+
+namespace PS.Mothership.Core.Common.Dto.Application
+{
+    [DataContract]
+    public class OpportunitySummaryDto
+    {
+        [DataMember]
+        public Guid OpportunityGuid { get; set; }
+
+        [DataMember]
+        public double Credit { get; set; }
+
+        [DataMember]
+        public double Debit { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Reference { get; set; }
+
+        [DataMember]
+        public string Status { get; set; }
+
+        [DataMember]
+        public string ContractLengthDescription { get; set; }
+
+
+    }
+}
