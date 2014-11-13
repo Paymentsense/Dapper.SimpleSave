5cdd88613ff2e189c4b5711386dbab29962e91e1
José Mira <jose.mira@paymentsense.com>
2014-11-13 17:50:51 +0000

    Add application status key to ApplicationSummaryDto

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationSummaryDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationSummaryDto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Template.Gen;
 
 namespace PS.Mothership.Core.Common.Dto.Application
 {
@@ -27,6 +28,9 @@ namespace PS.Mothership.Core.Common.Dto.Application
 
         [DataMember]
         public string Status { get; set; }
+
+        [DataMember]
+        public GenOpportunityStatusEnum ApplicationStatusKey { get; set; }
 
         [DataMember]
         public IList<LocationSummaryDto> Locations { get; set; }
