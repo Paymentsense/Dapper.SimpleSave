c52859d99fb64b25bbe017b18ac5749150415fa8
David Welch <david.welch@paymentsense.com>
2014-11-19 16:42:44 +0000

    Extra pieces of metadata for application opportunity and details pages

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationDetailsMetadataDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationDetailsMetadataDto.cs
@@ -2,12 +2,16 @@
 using System.Runtime.Serialization;
 using PS.Mothership.Core.Common.Template.App;
 using PS.Mothership.Core.Common.Template.Gen;
+using PS.Mothership.Core.Common.Dto.Merchant;
 
 namespace PS.Mothership.Core.Common.Dto.Application
 {
     [DataContract]
     public class ApplicationDetailsMetadataDto
     {
+        [DataMember]
+        public IEnumerable<OfferSalesChannelDto> SalesChannels { get; set; }
+
         [DataMember]
         public IList<GenBusinessLegalType> BusinessLegalTypes { get; set; }
 
