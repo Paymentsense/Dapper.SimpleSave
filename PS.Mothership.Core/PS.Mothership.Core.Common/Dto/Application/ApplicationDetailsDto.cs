0708101aa2ae62611b27c299b9c659a267725518
Alpesh Mistry <alpesh.mistry@paymentsense.com>
2014-11-21 17:32:25 +0000

    Squashed 'Externals/Core/' changes from 6a4ff4a..cab57d7
    cab57d7 Merge issue fixed.
    2400aa9 Merge branch 'Sprint5' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into 1181_Locations
    36b7369 Opportunity / Summary Dto update
    
    git-subtree-dir: Externals/Core
    git-subtree-split: cab57d7cab8ba71241f8e88c57aa49c18835aee9

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationDetailsDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationDetailsDto.cs
@@ -1,26 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Dto.Merchant;
+using PS.Mothership.Core.Common.Template.Gen;
 
 namespace PS.Mothership.Core.Common.Dto.Application
 {
     [DataContract]
-    public class ApplicationDetailLocationDto
+    public class ApplicationDetailsDto
     {
-        [DataMember]
-        public Guid LocationGuid { get; set; }
-
         [DataMember]
         public Guid ApplicationGuid { get; set; }
 
         [DataMember]
-        public FullAddressDto Address { get; set; }
-
-        //[DataMember]
-        public bool SiteVisitRequired { get; set; }
+        public LegalInfoDto LegalInfo { get; set; }
 
         [DataMember]
-        public IList<LocationProductMstDto> Opportunities { get; set; }
+        public ApplicationDetailLocationDto ApplicationDetailLocation { get; set; }
+
 
 
     }
