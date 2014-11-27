8b5636da859cca4922726c29875b49eae60068df
Alpesh Mistry <alpesh.mistry@paymentsense.com>
2014-11-27 12:15:04 +0000

    Application detail model refactoring.

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationDetailLocationProductDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationDetailLocationProductDto.cs
@@ -6,24 +6,26 @@ using System.Runtime.Serialization;
 namespace PS.Mothership.Core.Common.Dto.Application
 {
     [DataContract]
-    public class LocationMstDto
+    public class ApplicationDetailLocationDto
     {
         [DataMember]
         public Guid LocationGuid { get; set; }
 
         [DataMember]
-        public string LocationReference { get; set; }
+        public Guid ApplicationGuid { get; set; }
 
         [DataMember]
-        public Guid ApplicationGuid { get; set; }
+        public string LocationReference { get; set; }
 
         [DataMember]
         public string BusinessName { get; set; }
 
         [DataMember]
-        public Guid TradingAddressGuid { get; set; }
+        public FullAddressDto TradingAddress { get; set; }
 
         [DataMember]
-        public IList<LocationProductMstDto> LocationProducts { get; set; }
+        public IList<ApplicationDetailLocationProductDto> ApplicationDetailLocationProduct { get; set; }
+
+
     }
 }
--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationDetailsDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/ApplicationDetailsDto.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
-using PS.Mothership.Core.Common.Dto.Merchant;
-using PS.Mothership.Core.Common.Template.Gen;
 
 namespace PS.Mothership.Core.Common.Dto.Application
 {
@@ -16,9 +14,7 @@ namespace PS.Mothership.Core.Common.Dto.Application
         public LegalInfoDto LegalInfo { get; set; }
 
         [DataMember]
-        public IEnumerable<LocationMstDto> ApplicationDetailLocation { get; set; }
-
-
+        public IEnumerable<ApplicationDetailLocationDto> ApplicationDetailLocation { get; set; }
 
     }
 }
