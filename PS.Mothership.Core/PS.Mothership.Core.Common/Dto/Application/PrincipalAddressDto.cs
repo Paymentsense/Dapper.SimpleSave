801a6102f127895975f6f4aa62766ca7f73fdf28
Vipin Vijay <vipin.vijay@paymentsense.com>
2015-01-08 11:53:49 +0000

    Dto Update

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/PrincipalAddressDto.cs
@@ -0,0 +1,20 @@
+﻿using PS.Mothership.Core.Common.Template.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Dto.Application
+{
+    public class PrincipalAddressDto
+    {
+        public FullAddressDto Address { get; set; }
+
+        public AppPremisesOwnershipTypeEnum OwnershipType { get; set; }
+
+        public DateTime FromDate { get; set; }
+
+        public DateTime ToDate { get; set; }
+    }
+}
