51dccaec2fa031262d2d11366dd0a28ea5906705
Guy Butler <guy.butler@paymentsense.com>
2015-01-08 15:11:17 +0000

    Squashed 'Externals/Core/' changes from 8db6451..1a66f48
    1a66f48 campaign call dtos
    a98a37b Merge branch 'master' into Kraken_Sprint0
    3dae536 Dto Update
    77dadef Merge branch 'Sprint6' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core
    5006719 Merge branch 'Sprint6' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into 1312_Opportunity
    e79e288 Address lookup.
    05929c9 Application Details Opportunities
    132e992 Merge branch 'Sprint6' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into Server_Validation
    918f99f MCC related interfaces and data added.
    6ab0724 Added GetMerchantCategoryCodes method to IApplicationBoardingService.
    4f1e7ff Merge branch 'master' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into Sprint6
    63588ce Fixed bugs #1301 Opportunity header contract length.
    c683d72 Merge branch 'Sprint5' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into Server_Validation
    c9fdbe3 Updated merge errors
    ef9d8ab Merge branch 'Sprint5' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into Server_Validation
    274cf8c Merge branch 'Sprint5' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into Server_Validation
    2119a2b Merge branch 'Sprint5' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into Server_Validation
    cfd95db Updated Core
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 1a66f484e7f7d91b5eb35788813d9f455da24263

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Campaign/CampaignCallTrnDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Template.Dial;
+
+namespace PS.Mothership.Core.Common.Dto.Campaign
+{
+    public class CampaignCallTrnDto
+    {
+        [DataMember]
+        public Guid CampaignCallTrnGuid { get; set; }
+
+        [DataMember]
+        public Guid CallGuid { get; set; }
+
+        [DataMember]
+        public Guid PhoneNumberGuid { get; set; }
+
+        [DataMember]
+        public DialInboundCampaignCallResolutionEnum CampaignCallResolutionKey { get; set; }
+    }
+}
