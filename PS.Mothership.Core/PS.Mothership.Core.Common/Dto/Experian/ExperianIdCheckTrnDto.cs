5503e1cfec29d18c61bbc217e68451fd8b5819fc
Alpesh Mistry <alpesh.mistry@paymentsense.com>
2015-01-15 16:21:25 +0000

    Added ExperianIdCheckTrnDto to Core.

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Experian/ExperianIdCheckTrnDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Mothership.Core.Common.Dto.Experian
+{
+    [DataContract]
+    public class ExperianIdCheckTrnDto
+    {
+        [DataMember]
+        public Guid ExperianIdCheckGuid { get; set; }
+
+        [DataMember]
+        public Guid ContactGuid { get; set; }
+
+        [DataMember]
+        public IdentityCheckRequestDto IdentityCheckRequestDto { get; set; }
+
+        [DataMember]
+        public IdentityCheckResponseDto IdentityCheckResponseDto { get; set; }
+
+    }
+}
