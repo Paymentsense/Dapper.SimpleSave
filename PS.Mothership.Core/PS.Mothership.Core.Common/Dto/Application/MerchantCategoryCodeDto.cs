a33b24c4e8da512d36dc258afd5e30b6d42a58f2
José Mira <jose.mira@paymentsense.com>
2015-01-16 11:54:11 +0000

    Squashed 'Externals/Core/' changes from 132e992..7db49bc
    7db49bc Templated enumerations rebuild
    5856e7e App.Config pointing to PSMS_Scratch
    3dae536 Dto Update
    77dadef Merge branch 'Sprint6' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core
    5006719 Merge branch 'Sprint6' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into 1312_Opportunity
    e79e288 Address lookup.
    05929c9 Application Details Opportunities
    918f99f MCC related interfaces and data added.
    6ab0724 Added GetMerchantCategoryCodes method to IApplicationBoardingService.
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 7db49bce225c2a82a6a0b383aac2ccfe7b64e033

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Application/MerchantCategoryCodeDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace PS.Mothership.Core.Common.Dto.Application
+{
+    [DataContract]
+    public class MerchantCategoryCodeDto
+    {
+        [DataMember]
+        public int MccKey { get; set; }
+
+        [DataMember]
+        public int MCCCategoryKey { get; set; }
+
+        [DataMember]
+        public string Mcc { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Category { get; set; }
+    }
+}
