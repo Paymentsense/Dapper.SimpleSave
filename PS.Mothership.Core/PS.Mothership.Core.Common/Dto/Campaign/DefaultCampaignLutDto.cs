5728397a3a0b2e31d8b4ab2cab479f89433a1be6
Guy Butler <guy.butler@paymentsense.com>
2015-01-19 13:36:48 +0000

    Squashed 'Externals/Core/' changes from 0f9db63..7d77f31
    7d77f31 defaultcampaignLut countrykey
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 7d77f31827bf9f8277d09c69bd8ecf2d9d8d5167

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Campaign/DefaultCampaignLutDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Campaign/DefaultCampaignLutDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Template.Gen;
 
 namespace PS.Mothership.Core.Common.Dto.Campaign
 {
@@ -22,5 +23,8 @@ namespace PS.Mothership.Core.Common.Dto.Campaign
 
         [DataMember]
         public string Url { get; set; }
+
+        [DataMember]
+        public GenCountryEnum CountryKey { get; set; }
     }
 }
