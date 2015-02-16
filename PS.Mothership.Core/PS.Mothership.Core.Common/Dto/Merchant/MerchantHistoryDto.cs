8517594a03e7c5d7d812a467955cf32d3d8fcb22
Adrian Lote <adrian.lote@paymentsense.com>
2015-02-16 09:14:30 +0000

    Squashed 'Externals/Core/' changes from 474196a..23dd5d8
    23dd5d8 Las Access Time DTO change
    db75da5 Merge branch 'Kraken_ScheduledCallBacks' into Kraken_LastViewedMerchants
    ec08fde New DTO - MerchantHistoryDto
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 23dd5d82703614a95b588d5ac9e7733ae52cdfcc

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/MerchantHistoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PS.Mothership.Core.Common.Dto.Merchant
+{
+    [DataContract]
+    public class MerchantHistoryDto
+    {
+        [DataMember]
+        public string BusinessName { get; set; }
+
+        [DataMember]
+        public Guid MerchantGuid { get; set; }
+
+        [DataMember]
+        public DateTimeOffset? LastAccessTime { get; set; }
+    }
+}
