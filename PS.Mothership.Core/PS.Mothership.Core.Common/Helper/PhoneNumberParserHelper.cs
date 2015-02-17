2526634f1dae2fbcde58071197f341bc8f8d31e7
José Mira <jose.mira@paymentsense.com>
2015-02-17 15:18:37 +0000

    Squashed 'Externals/Core/' changes from a00cbee..89d0516
    89d0516 Moved phone number parsed into Core
    3c61759 Moved phone number parsed into Core
    59d7eb0 Moved phone number parsed into Core
    c33b893 Merge branch 'Kraken_Sprint2' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into 1441_CallLogMerchantName
    dc7febf Get call log descriptions asynchronously
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 89d0516d4fda3fb29471a96735d229da35538073

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Helper/PhoneNumberParserHelper.cs
@@ -0,0 +1,62 @@
+﻿using libphonenumber;
+using PS.Mothership.Core.Common.Dto.Dialler;
+using PS.Mothership.Core.Common.Template.Gen;
+using System.Linq;
+
+namespace PS.Mothership.Core.Common.Helper
+{
+    public class PhoneNumberParserHelper
+    {
+        public static ParsedPhoneNumberDto Parse(string phoneNumber)
+        {
+            var ret = new ParsedPhoneNumberDto
+            {
+                Country = GenCountryEnum.None,
+                PhoneNumber = ""
+            };
+
+            if (!string.IsNullOrEmpty(phoneNumber))
+            {
+                var country = GenCountryEnum.UnitedKingdom;
+                var significantPhoneNumber = phoneNumber;
+                if (phoneNumber.StartsWith("00") || phoneNumber.StartsWith("+")) // international number
+                {
+                    PhoneNumber phoneNumberParse = null;
+                    try
+                    {
+                        // using libphonenumber to parse an international phone number and split it
+                        // into its components (https://libphonenumber.codeplex.com/)
+                        // source code is in \\ps-storage01\PSData\DEVELOPERS\External DLLs Source Code\libphonenumber
+                        phoneNumberParse =
+                            PhoneNumberUtil.Instance.Parse(
+                                phoneNumber.StartsWith("00")
+                            // parser only works correctly if international number starts with +, not 00
+                                    ? string.Format("+{0}", phoneNumber.Substring(2))
+                                    : phoneNumber, null);
+
+                        GenCountry genCountry = GenCountryCollection.GenCountryList.FirstOrDefault(
+                            x => x.Alpha2CountryCode == phoneNumberParse.RegionCodeForNumber);
+
+                        country = genCountry != null ? (GenCountryEnum)genCountry.CountryKey : GenCountryEnum.None;
+                    }
+                    catch
+                    {
+                        country = GenCountryEnum.None;
+                    }
+                    finally
+                    {
+                        if (phoneNumberParse != null && country != GenCountryEnum.None)
+                        {
+                            significantPhoneNumber = phoneNumberParse.NationalSignificantNumber;
+                        }
+                    }
+                }
+
+                ret.Country = country;
+                ret.PhoneNumber = significantPhoneNumber;
+            }
+
+            return ret;
+        }
+    }
+}
