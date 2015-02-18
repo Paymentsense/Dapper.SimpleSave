ad60213abdef311f56eb9dbfb9208b5a0df4eaf8
Guy Butler <guy.butler@paymentsense.com>
2015-02-18 11:02:04 +0000

    dialler service callback config

--- PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IDiallerServiceCallback.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IDiallerServiceCallback.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ServiceModel;
 
 namespace PS.Mothership.Core.Common.Contracts
 {
     public interface IDiallerServiceCallback
     {
+        [OperationContract(IsOneWay = true)]
         bool CheckUserAvailability(Guid userGuid);
     }
 }
