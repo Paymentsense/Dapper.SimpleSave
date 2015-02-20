0d7bf291edc0dda09afa79a299ba48512eaf36c5
Guy Butler <guy.butler@paymentsense.com>
2015-02-20 10:29:26 +0000

    Squashed 'Externals/Core/' changes from 89d0516..b4df337
    b4df337 removed dialler service callback and add new dialler task service with a callback
    6c2edbc Merge branch 'Kraken_Sprint2' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into 1363_TriggerScheduledCallback
    3bd7c8d Merge branch 'Kraken_ScheduledCallBacks' into Kraken_LastViewedMerchants
    14076e4 Merge branch 'Kraken_Sprint2' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into Kraken_ScheduledCallBacks
    7bd0d83 Merge pull request #39 in MS2/mothershipv2_core from 1441_CallLogMerchantName to Kraken_Sprint2
    9cea325 remove is one way
    eaa865f diallerservice callback operation contract
    c04dc6c Dialler service callback
    d02ac55 Merge branch 'Kraken_ScheduledCallBacks' into Kraken_LastViewedMerchants
    5df4888 Merchant History DTO updates
    5132ac1 Merge branch 'Kraken_Sprint2' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into Kraken_ScheduledCallBacks
    9207a79 dialler task notification contact changes
    8e85afe delete callback method interface change
    23dd5d8 Las Access Time DTO change
    882e581 ContactEventLnkDto
    db75da5 Merge branch 'Kraken_ScheduledCallBacks' into Kraken_LastViewedMerchants
    474196a Overload added in DiallerTaskNotification
    ec08fde New DTO - MerchantHistoryDto
    6650fba Merge branch 'Kraken_ScheduledCallBacks' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into Kraken_ScheduledCallBacks
    1d86d3a ScheduledCallbackDto changed
    52e02b0 reschedule task
    c7e89f7 add phone number guid to scheduled call back
    9fa32a4 metadata changes
    f6fc924 scheduled callback metadata
    419c71f dialler task notification service
    a05203a contact Guid added to schedulecallback dto
    fb4fc02 Dialler TaskNotification interface update
    d3dffdc merchant guid added to scheduled call back dto
    d3b10cd Added ScheduledCallbackDtos
    
    git-subtree-dir: Externals/Core
    git-subtree-split: b4df337847bd4eacaf925de3f68108572ec7ce58

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IDiallerTaskService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace PS.Mothership.Core.Common.Contracts
+{
+    [ServiceContract(CallbackContract = typeof(IDiallerTaskServiceCallback))]
+    public interface IDiallerTaskService
+    {
+        //This is only here because wcf needs atleast one operation contract per service contract
+        [OperationContract]
+        void Post();
+    }
+}
---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/ScheduledCallBack/ScheduledCallBackMetaDataDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using PS.Mothership.Core.Common.Template.Event;
+
+namespace PS.Mothership.Core.Common.Dto.ScheduledCallBack
+{
+    [DataContract]
+    public class ScheduledCallbackMetaDataDto
+    {
+        [DataMember]
+        public IList<EventType> EventTypes { get; set; }
+
+        [DataMember]
+        public IList<ScheduledCallbackContactMetaDataDto> MerchantContacts { get; set; }
+    }
+}
