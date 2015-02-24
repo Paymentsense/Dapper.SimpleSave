7b31a9519cb8aae90c90f0450b674602c90edb82
Guy Butler <guy.butler@paymentsense.com>
2015-02-24 09:41:26 +0000

    Squashed 'Externals/Core/' changes from f74ad52..9dadb39
    9dadb39 Auto Reschedule Dto
    0403f06 update dialertaskservice
    da00d4b removed dialler service callback and add new dialler task service with a callback
    31c720e remove is one way
    10f9a72 diallerservice callback operation contract
    40dbd1e Dialler service callback
    34cb54c Merge pull request #40 in MS2/mothershipv2_core from Kraken_Sprint2 to master
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 9dadb39097ba73ad5dc4719e77619733b46fdbec

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Contracts/IDiallerTaskService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace PS.Mothership.Core.Common.Contracts
+{
+    [ServiceContract(CallbackContract = typeof(IDiallerTaskServiceCallback))]
+    public interface IDiallerTaskService
+    {
+        [OperationContract(IsOneWay = false)]
+        void Subscribe(string applicationName);
+
+        [OperationContract(IsOneWay = true)]
+        void EndSubscribe(string applicationName);
+    }
+}
