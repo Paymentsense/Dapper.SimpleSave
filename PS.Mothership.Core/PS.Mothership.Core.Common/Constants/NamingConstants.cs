69fb83588c6f3f19bb102a5e87fd8550e10469d5
Guy Butler <guy.butler@paymentsense.com>
2015-02-24 10:29:12 +0000

    Squashed 'Externals/Core/' changes from 9dadb39..b53f727
    b53f727 Dialler application Const added
    
    git-subtree-dir: Externals/Core
    git-subtree-split: b53f727e2a69d17c2aa7f2780b63294eb6b72959

--- PS.Mothership.Core/PS.Mothership.Core.Common/Constants/NamingConstants.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Constants/NamingConstants.cs
@@ -11,5 +11,6 @@ namespace PS.Mothership.Core.Common.Constants
         public const string ApplicationName = "PS.WEB.UI";
         public const double SendMessageMaxErrorDelayDefault = 5;
         public const int SendMessageMaxAttemptsDefault = 16;
+        public const string DiallerApplicationName = "Dialler";
     }
 }
