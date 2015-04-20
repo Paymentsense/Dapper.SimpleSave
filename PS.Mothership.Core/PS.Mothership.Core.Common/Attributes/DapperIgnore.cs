3f4b5c3e653855fd88c847ad9e442f1ca5a7d1fb
Guy Butler <guy.butler@paymentsense.com>
2015-04-20 16:06:57 +0100

    Squashed 'Externals/Core/' changes from 309c327..70a1c0d
    70a1c0d Merge branch 'develop' of http://git.mothershipdev.com:7990/scm/ms2/mothershipv2_core into feature/MS-222-inbound-call-resolution-hot-calls
    01050d3 Added Serializable attribute
    575f258 MerchantDto Update
    8bd4b0f Dapper Core Updates
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 70a1c0d79089d753660328daf1db52ca2a19755d

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Attributes/DapperIgnore.cs
@@ -0,0 +1,12 @@
+﻿namespace PS.Mothership.Core.Common.Attributes
+{
+    ﻿using System;
+
+    namespace PS.Mothership.Core.Common.Attributes
+    {
+        [AttributeUsage(AttributeTargets.Property)]
+        public class DapperIgnore : Attribute
+        {
+        }
+    }
+}
