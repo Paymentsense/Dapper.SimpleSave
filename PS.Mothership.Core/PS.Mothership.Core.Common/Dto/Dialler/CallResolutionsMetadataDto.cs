2b28004a2562db30e111ae76dc63725722a899b9
Guy Butler <guy.butler@paymentsense.com>
2015-04-23 09:59:43 +0100

    Squashed 'Externals/Core/' changes from 70a1c0d..d69e4d3
    d69e4d3 call res metadatadto change
    
    git-subtree-dir: Externals/Core
    git-subtree-split: d69e4d343bed34a137a108c9d8a81d47db9fe6b4

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Dialler/CallResolutionsMetadataDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Dialler/CallResolutionsMetadataDto.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
-using PS.Mothership.Core.Common.Dto.ScheduledCallBack;
 using PS.Mothership.Core.Common.Template.Mrkt;
 
 namespace PS.Mothership.Core.Common.Dto.Dialler
@@ -8,8 +7,6 @@ namespace PS.Mothership.Core.Common.Dto.Dialler
     [DataContract]
     public class CallResolutionsMetadataDto
     {
-        public ScheduledCallbackMetaDataDto TaskMetadeta { get; set; }
-
         public IEnumerable<MrktCampaignCallResolution> CampaignCallResolutions { get; set; }
 
         //TODO: This needs to be changed to the new non-campaign call resolution type
