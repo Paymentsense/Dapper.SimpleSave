79d7c5416c2a525754667bf5b23ea58258a1ff58
Guy Butler <guy.butler@paymentsense.com>
2015-04-23 12:18:32 +0100

    Squashed 'Externals/Core/' changes from d69e4d3..d6caa0e
    d6caa0e data member
    
    git-subtree-dir: Externals/Core
    git-subtree-split: d6caa0ef9c6e8f961849854cb6e23cb118926a7b

--- PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Dialler/CallResolutionsMetadataDto.cs
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Dialler/CallResolutionsMetadataDto.cs
@@ -7,9 +7,11 @@ namespace PS.Mothership.Core.Common.Dto.Dialler
     [DataContract]
     public class CallResolutionsMetadataDto
     {
+        [DataMember]
         public IEnumerable<MrktCampaignCallResolution> CampaignCallResolutions { get; set; }
 
         //TODO: This needs to be changed to the new non-campaign call resolution type
+        [DataMember]
         public IEnumerable<int> CallResolutions { get; set; }
     }
 }
