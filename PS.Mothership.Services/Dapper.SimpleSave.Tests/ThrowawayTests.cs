fed8b0b35e4b9460859908e7117408ab14bbf1ff
unknown <bart.read@84P1852.pswestbourne.com>
2015-05-21 11:47:25 +0100

    MS-398 Added checks on operation and command counts to tests. Also added another test to deal with a more complex set of INSERTs and DELETEs on a user's 'sub-objects'

--- PS.Mothership.Services/Dapper.SimpleSave.Tests/ThrowawayTests.cs
+++ PS.Mothership.Services/Dapper.SimpleSave.Tests/ThrowawayTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -81,20 +82,65 @@ namespace Dapper.SimpleSave.Tests
                 })
         };
 
+        private static readonly UserDto ZargonComplexUpdates = new UserDto() {
+            UserKey = 1,
+            FirstName = "Zargon",
+            LastName = "Smith",
+            PhoneNumber = "0207 666 6666",
+            EmailAddress = "john.smith@paymentsense.com",
+            Username = "zargon.smith",
+            Department = new List<DepartmentDto>(new []
+            {
+                new DepartmentDto
+                {
+                    DepartmentKey = 2,
+                    Name = "The Pancake Landing Squad"
+                },
+                new DepartmentDto
+                {
+                    DepartmentKey = 3,
+                    Name = "Zombie Hunters"
+                }
+            }),
+            Team = new List<TeamDto>(new []
+            {
+                new TeamDto
+                {
+                    TeamKey = 1,
+                    Name = "The A-Team"
+                }
+            }),
+            AdditionalRoles = new List<RoleDto>(new []
+            {
+                new RoleDto
+                {
+                    RoleKey = 1,
+                    Name = "Howling Mad Murdoch Replacement"
+                }
+            })
+        };
+
         private static UserDto GetDto(string name)
         {
             var field = typeof (ThrowawayTests).GetField(name, BindingFlags.NonPublic | BindingFlags.Static);
             return (UserDto) field.GetValue(null);
         }
 
-        [TestCase("JohnSmith", "ZargonRemovedDepartment", 4, 4, 2)]
-        [TestCase("JohnSmith", "ZargonAddedDepartment", 4, 4, 2)]
+        [TestCase("JohnSmith", "ZargonRemovedDepartment", 4, 4, 0, 3, 1, 2, 0, 1, 1)]
+        [TestCase("JohnSmith", "ZargonAddedDepartment", 4, 4, 1, 3, 0, 2, 1, 1, 0)]
+        [TestCase("JohnSmith", "ZargonComplexUpdates", 7, 7, 3, 3, 1, 5, 3, 1, 1)]
         public void multi_level_updates_generates_correct_sql(
             string oldUserFieldName,
             string newUserFieldName,
             int expectedDifferenceCount,
             int expectedOperationCount,
-            int expectedCommandCount)
+            int expectedInsertOperations,
+            int expectedUpdateOperations,
+            int expectedDeleteOperations,
+            int expectedCommandCount,
+            int expectedInsertCommands,
+            int expectedUpdateCommands,
+            int expectedDeleteCommands)
         {
             var cache = new DtoMetadataCache();
             var differ = new Differ(cache);
@@ -107,12 +153,41 @@ namespace Dapper.SimpleSave.Tests
             var commands = operationBuilder.Coalesce(operations);
 
             Assert.AreEqual(expectedOperationCount, operations.Count(), "Unexpected number of operations.");
+            var counts = CountItemsByType(operations);
+            CheckCount(counts, typeof(InsertOperation), expectedInsertOperations);
+            CheckCount(counts, typeof(UpdateOperation), expectedUpdateOperations);
+            CheckCount(counts, typeof(RemoveOperation), expectedDeleteOperations);
+
             Assert.AreEqual(expectedCommandCount, commands.Count(), "Unexpected number of commands.");
+            counts = CountItemsByType(commands);
+            CheckCount(counts, typeof(InsertCommand), expectedInsertCommands);
+            CheckCount(counts, typeof(UpdateCommand), expectedUpdateCommands);
+            CheckCount(counts, typeof(DeleteCommand), expectedDeleteCommands);
 
             var scriptBuilder = new ScriptBuilder(cache);
             var transactionScript = scriptBuilder.BuildTransaction(commands);
 
             Assert.IsNotNull(transactionScript, "#badtimes - null transaction script");
+            Assert.IsTrue(transactionScript.Length > 0, "#badtimes - empty transaction script");
+        }
+
+        private void CheckCount(IDictionary<Type, int> counts, Type type, int expectedCount)
+        {
+            Assert.AreEqual(
+                expectedCount,
+                counts.ContainsKey(type) ? counts[type] : 0,
+                string.Format("Unexpected count for {0}", type));
+        }
+
+        private IDictionary<Type, int> CountItemsByType(IEnumerable items)
+        {
+            var results = new Dictionary<Type, int>();
+            foreach (var item in items)
+            {
+                Type type = item.GetType();
+                results[type] = results.ContainsKey(type) ? results[type] + 1 : 1;
+            }
+            return results;
         }
     }
 }
