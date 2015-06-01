d6f2a7795d644e5dff31d6b5c3f775074e5125a1
unknown <bart.read@84P1852.pswestbourne.com>
2015-06-01 18:37:33 +0100

    MS-398 Stubbed out tests for one to one relationships.

---
+++ PS.Mothership.Services/Dapper.SimpleSave.Tests/OneToOneRelationshipFkOnChildCommandGenerationTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Dapper.SimpleSave.Tests {
+
+    [TestFixture]
+    public class OneToOneRelationshipFkOnChildCommandGenerationTests {
+        [Test]
+        public void insert_with_fk_on_child_no_reference_data_inserts_rows_in_parent_and_child() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void update_with_fk_on_child_no_reference_data_updates_rows_in_parent_and_child() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void delete_with_fk_on_child_no_reference_data_deletes_rows_in_child_and_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void insert_with_fk_on_child_and_reference_data_in_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void update_with_fk_on_child_and_reference_data_in_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void delete_with_fk_on_child_and_reference_data_in_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void insert_with_fk_on_child_and_special_data_in_child_inserts_in_parent_and_updates_child() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void update_with_fk_on_child_and_special_data_in_child_updates_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void delete_with_fk_on_child_and_special_data_in_child_updates_child_and_deletes_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void insert_with_fk_on_child_and_reference_data_in_parent_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void update_with_fk_on_child_and_reference_data_in_parent_updates_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void delete_with_fk_on_child_and_reference_data_in_parent_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void insert_with_fk_on_child_and_reference_data_in_both_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void update_with_fk_on_child_and_reference_data_in_both_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void delete_with_fk_on_child_and_reference_data_in_both_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PS.Mothership.Services/Dapper.SimpleSave.Tests/OneToOneRelationshipFkOnParentCommandGenerationTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Dapper.SimpleSave.Tests {
+
+    [TestFixture]
+    public class OneToOneRelationshipFkOnParentCommandGenerationTests
+    {
+        [Test]
+        public void insert_with_fk_on_parent_no_reference_data_inserts_rows_in_child_and_parent()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void update_with_fk_on_parent_no_reference_data_updates_rows_in_child_and_parent()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void delete_with_fk_on_parent_no_reference_data_deletes_rows_in_parent_and_child()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void insert_with_fk_on_parent_and_reference_data_in_child_inserts_in_parent_only()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void update_with_fk_on_parent_and_reference_data_in_child_updates_in_parent_only() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void delete_with_fk_on_parent_and_reference_data_in_child_deletes_in_parent_only() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void insert_with_fk_on_parent_and_reference_data_in_parent_is_not_supported()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void update_with_fk_on_parent_and_reference_data_in_parent_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void delete_with_fk_on_parent_and_reference_data_in_parent_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void insert_with_fk_on_parent_and_special_data_in_parent_is_not_supported()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void update_with_fk_on_parent_and_special_data_in_parent_updates_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void delete_with_fk_on_parent_and_special_data_in_parent_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void insert_with_fk_on_parent_and_reference_data_in_both_parent_and_child_is_not_supported()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void update_with_fk_on_parent_and_reference_data_in_both_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void delete_with_fk_on_parent_and_reference_data_in_both_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+    }
+}
