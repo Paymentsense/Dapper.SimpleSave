f6569ea0b3933da0838aeed90172b395debd6b2d
unknown <bart.read@84P1852.pswestbourne.com>
2015-06-01 19:12:17 +0100

    MS-398 Stubbed out many to many relationship tests.

---
+++ PS.Mothership.Services/Dapper.SimpleSave.Tests/ManyToManyRelationshipCommandGenerationTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Dapper.SimpleSave.Tests {
+
+    [TestFixture]
+    public class ManyToManyRelationshipCommandGenerationTests {
+
+        [Test]
+        public void insert_with_no_reference_data_inserts_in_parent_and_link_table()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void update_with_no_reference_data_updates_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void delete_with_no_reference_data_deletes_from_link_table_and_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void insert_with_reference_data_in_child_inserts_in_parent_and_link_table() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void update_with_reference_data_in_child_updates_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void delete_with_reference_data_in_child_deletes_from_link_table_and_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void insert_with_special_data_in_child_inserts_in_parent_and_link_table() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void update_with_special_data_in_child_updates_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void delete_with_special_data_in_child_deletes_from_link_table_and_parent() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void insert_with_reference_data_in_parent_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void update_with_reference_data_in_parent_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void delete_with_reference_data_in_child_parent_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void insert_with_reference_data_in_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void update_with_reference_data_in_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void delete_with_reference_data_in_child_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void insert_with_special_data_in_parent_and_reference_data_in_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void update_with_special_data_in_parent_and_reference_data_in_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void delete_with_special_data_in_parent_and_reference_data_in_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void insert_with_special_data_in_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void update_with_special_data_in_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void delete_with_special_data_in_child_parent_and_child_is_not_supported() {
+            throw new NotImplementedException();
+        }
+    }
+}
