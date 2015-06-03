4046357338dcd9d7cf58f9bcd2e21757492f93a1
José Mira <jose.mira@paymentsense.com>
2015-06-03 11:55:05 +0100

    Squashed 'Externals/Core/' changes from 2959a9d..4ef1f4e
    4ef1f4e fix removed needed file
    
    git-subtree-dir: Externals/Core
    git-subtree-split: 4ef1f4e510523ed40468051e0dc2bab94a0acb5b

---
+++ PS.Mothership.Core/PS.Mothership.Core.Common/Dto/Merchant/OfferDebitPriorityDto.cs
@@ -0,0 +1,41 @@
+﻿﻿using System.Runtime.Serialization;
+
+namespace PS.Mothership.Core.Common.Dto.Merchant
+{
+    [DataContract]
+    public class OfferDebitPriorityDto
+    {
+        [DataMember]
+        public int DebitPriorityKey { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public double Value { get; set; }
+
+        [DataMember]
+        public int DisplayOrder { get; set; }
+
+        [DataMember]
+        public double SelectedDefault { get; set; }
+
+        [DataMember]
+        public double SelectedFloor { get; set; }
+
+        [DataMember]
+        public double SelectedCeiling { get; set; }
+
+        [DataMember]
+        public double DeclinedDefault { get; set; }
+
+        [DataMember]
+        public double DeclinedFloor { get; set; }
+
+        [DataMember]
+        public double DeclinedCeiling { get; set; }
+    }
+}
