63215738adea6fbd7cdc437e20b08ec77c9fdb4c
Bart Read <bart@bartread.com>
2015-06-10 14:42:13 +0100

    MS-587 Attempt to fix filename, which I don't think is going to work.

--- PS.Mothership.Services/Dapper.SimpleSave/Impl/UpDateOperation.cs
+++ PS.Mothership.Services/Dapper.SimpleSave/Impl/UpDateOperation.cs
@@ -1,7 +1,7 @@
 ﻿namespace Dapper.SimpleSave.Impl
 {
     /// <summary>
-    /// Represents an update operation to a single column in a table. Multiple
+    /// Represents an update operation to carry out on a single column in a table. Multiple
     /// such operations may be coalesced together into a single <see cref="UpdateCommand"/>,
     /// which results in the generation of a single <code>UPDATE</code> SQL statement
     /// that updates multiple columns.
