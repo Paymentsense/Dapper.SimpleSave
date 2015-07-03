8e03266b30f4817f39024636ca43769744223615
Bart Read <bart@bartread.com>
2015-07-03 18:55:42 +0100

    Weeeelll.... I can't find the bug. But I did find a problem with the tests in that one-to-one and one-to-many relationships should have been modelled using collections. I've added a specific test for Deepti's problem and - annoyingly - it passes, which means I'm going to need to take another look at what she's doing, when I can find the right chunk of code. I've also pulled the code that builds the scripts out into a transaction builder, for easier testability. I know we don't generally want to test internals, but this code is quite key to the safe and correct operation of the whole of Mothership, so we should probably make an exception.

---
+++ PS.Mothership.Services/Dapper.SimpleSave.Tests/OneToManyRelationshipScriptGenerationTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.SimpleSave.Impl;
+using Dapper.SimpleSave.Tests.Dto;
+using NUnit.Framework;
+
+namespace Dapper.SimpleSave.Tests
+{
+    [TestFixture]
+    public class OneToManyRelationshipScriptGenerationTests
+    {
+        [Test]
+        public void insert_with_no_reference_data_inserts_in_parent_and_child()
+        {
+            var newDto = new ParentDto {
+                OneToManyChildDto = new [] {new OneToManyChildDto()}
+            };
+
+            var cache = new DtoMetadataCache();
+            var builder = new TransactionBuilder(cache);
+            var scripts = builder.BuildUpdateScripts(null, newDto);
+            
+            Assert.AreEqual(
+                2,
+                scripts.Count,
+                "Unexpected number of scripts.");
+
+            Assert.IsTrue(
+                scripts[0].Buffer.ToString().Contains("INSERT INTO dbo.[Parent]"),
+                "Should INSERT into parent table.");
+
+            Assert.IsTrue(
+                scripts[0].Buffer.ToString().Contains("INSERT INTO dbo.OneToManyChild"),
+                "Should INSERT into child table.");
+        }
+    }
+}
