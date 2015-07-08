16b60f80e013833b75523b857f8aad4c3122f30b
Bart Read <bart@bartread.com>
2015-07-08 15:50:41 +0100

    MS-706 Stubbed out test to investigate script generation for 1:1 relationships where the FK column is on the parent table.

---
+++ PS.Mothership.Services/Dapper.SimpleSave.Tests/OneToOneRelationshipFkOnParentScriptGenerationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.SimpleSave.Tests.Dto;
+using NUnit.Framework;
+
+namespace Dapper.SimpleSave.Tests
+{
+
+    [TestFixture]
+    public class OneToOneRelationshipFkOnParentScriptGenerationTests
+    {
+        public void insert_with_fk_on_parent_no_reference_data_inserts_rows_in_child_and_parent()
+        {
+            var newDto = new ParentDto
+            {
+                OneToOneChildDtoNoFk = new OneToOneChildDtoNoFk()
+            };
+
+            insert_maybe_inserts_in_child_and_always_in_parent(newDto, typeof(OneToOneChildDtoNoFk), true);
+        }
+    }
+}
