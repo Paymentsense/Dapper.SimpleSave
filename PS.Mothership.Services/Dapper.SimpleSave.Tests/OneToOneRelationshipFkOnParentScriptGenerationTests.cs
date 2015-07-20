eceda60a3a81a49e26addafad8522be6bf28c1b1
Bart Read <bart@bartread.com>
2015-07-20 17:08:39 +0100

    MS-742 Fixed UPDATE bug on one to one relationships with FK in parent table and bolstered test cases around this.

--- PS.Mothership.Services/Dapper.SimpleSave.Tests/OneToOneRelationshipFkOnParentScriptGenerationTests.cs
+++ PS.Mothership.Services/Dapper.SimpleSave.Tests/OneToOneRelationshipFkOnParentScriptGenerationTests.cs
@@ -1,4 +1,7 @@
-﻿using Dapper.SimpleSave.Tests.Dto;
+﻿using System;
+using System.Collections.Generic;
+using Dapper.SimpleSave.Impl;
+using Dapper.SimpleSave.Tests.Dto;
 using Dapper.SimpleSave.Tests.GuidDtos;
 using NUnit.Framework;
 
@@ -24,6 +27,27 @@ namespace Dapper.SimpleSave.Tests
         }
 
         [Test]
+        public void delete_with_fk_on_parent_no_reference_data_deletes_rows_in_parent_and_child()
+        {
+            var scripts = Generate(
+                new ParentDto
+                {
+                    ParentKey = 1,
+                    OneToOneChildDtoNoFk = new OneToOneChildDtoNoFk
+                    {
+                        ChildKey = 2
+                    }
+                },
+                null,
+                1);
+
+            AssertOnDeletes(
+                scripts,
+                "DELETE FROM dbo.[Parent]",
+                "DELETE FROM dbo.OneToOneChildNoFk");
+        }
+
+        [Test]
         public void insert_with_fk_on_parent_no_reference_data_inserts_rows_in_child_and_parent_with_guid_pks()
         {
             var scripts = Generate(
@@ -36,6 +60,37 @@ namespace Dapper.SimpleSave.Tests
 
             scripts.AssertFragment(0, "INSERT INTO dbo.GuidOneToOneChildNoFk");
             scripts.AssertFragment(1, "INSERT INTO dbo.[GuidParent]");
+        }
+
+        [Test]
+        public void delete_with_fk_on_parent_no_reference_data_deletes_rows_in_parent_and_child_with_guid_pks()
+        {
+            var scripts = Generate(
+                new GuidParentDto
+                {
+                    ParentKey = Guid.NewGuid(),
+                    OneToOneChildDtoNoFk = new GuidOneToOneChildDtoNoFk
+                    {
+                        ChildKey = Guid.NewGuid()
+                    }
+                },
+                null,
+                1);
+
+            AssertOnDeletes(
+                scripts,
+                "DELETE FROM dbo.[GuidParent]",
+                "DELETE FROM dbo.GuidOneToOneChildNoFk");
+        }
+
+        private static void AssertOnDeletes(IList<Script> scripts, string parentDeleteSqlFragment, string childDeleteSqlFragment)
+        {
+            var parentIndex = scripts.AssertFragment(0, parentDeleteSqlFragment);
+            var childIndex = scripts.AssertFragment(0, childDeleteSqlFragment);
+
+            Assert.IsTrue(
+                parentIndex < childIndex,
+                string.Format("Parent should be DELETEd before child in script: {0}", scripts[0].Buffer));
         }
 
         [Test]
@@ -57,7 +112,35 @@ namespace Dapper.SimpleSave.Tests
                 2);
 
             scripts.AssertFragment(0, "INSERT INTO dbo.OneToOneChildNoFk");
-            scripts.AssertFragment(1, "UPDATE dbo.[Parent] SET");
+            scripts.AssertFragment(1, @"UPDATE dbo.[Parent]
+SET [OneToOneChildDtoNoFk]");
+        }
+
+        [Test]
+        public void delete_child_with_fk_on_parent_and_deletes_child_and_updates_parent()
+        {
+            var scripts = Generate(
+                new ParentDto
+                {
+                    ParentKey = 1,
+                    OneToOneChildDtoNoFk = new OneToOneChildDtoNoFk
+                    {
+                        ChildKey = 2
+                    }
+                },
+                new ParentDto
+                {
+                    ParentKey = 1,
+                },
+                1);
+
+            var updateIndex = scripts.AssertFragment(0, @"UPDATE dbo.[Parent]
+SET [OneToOneChildDtoNoFk]");
+            var deleteIndex = scripts.AssertFragment(0, "DELETE FROM dbo.OneToOneChildNoFk");
+
+            Assert.IsTrue(
+                updateIndex < deleteIndex,
+                string.Format("UPDATE should appear before DELETE in script: {0}", scripts[0].Buffer));
         }
 
         [Test]
@@ -78,7 +161,8 @@ namespace Dapper.SimpleSave.Tests
                 },
                 1);
 
-            scripts.AssertFragment(1, "UPDATE dbo.[Parent] SET");
+            scripts.AssertFragment(0, @"UPDATE dbo.[Parent]
+SET [OneToOneReferenceChildDtoNoFk]");
         }
     }
 }
--- PS.Mothership.Services/Dapper.SimpleSave.Tests/ScriptsAssertions.cs
+++ PS.Mothership.Services/Dapper.SimpleSave.Tests/ScriptsAssertions.cs
@@ -6,15 +6,18 @@ namespace Dapper.SimpleSave.Tests
 {
     internal static class ScriptsAssertions
     {
-        public static void AssertFragment(this IList<Script> scripts, int scriptIndex, string expectedFragment)
+        public static int AssertFragment(this IList<Script> scripts, int scriptIndex, string expectedFragment)
         {
             var sql = scripts[scriptIndex].Buffer.ToString();
+            var index = sql.IndexOf(expectedFragment);
             Assert.IsTrue(
-                sql.Contains(expectedFragment),
+                index >= 0,
                 string.Format("Expected fragment '{0}' not found in script at index {1}: {2}",
                 expectedFragment,
                 scriptIndex,
                 sql));
+
+            return index;
         }
     }
 }
