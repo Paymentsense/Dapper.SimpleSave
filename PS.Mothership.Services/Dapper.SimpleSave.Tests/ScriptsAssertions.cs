a86afa1ce9a6f793735517d2e55a5174e55a145b
Bart Read <bart@bartread.com>
2015-07-20 11:53:21 +0100

    Fixed and added tests to cover INSERT and DELETE scenarios in MS-741 to deal with bug found by Richard and Agata.

---
+++ PS.Mothership.Services/Dapper.SimpleSave.Tests/ScriptsAssertions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Dapper.SimpleSave.Impl;
+using NUnit.Framework;
+
+namespace Dapper.SimpleSave.Tests
+{
+    internal static class ScriptsAssertions
+    {
+        public static void AssertFragment(this IList<Script> scripts, int scriptIndex, string expectedFragment)
+        {
+            var sql = scripts[scriptIndex].Buffer.ToString();
+            Assert.IsTrue(
+                sql.Contains(expectedFragment),
+                string.Format("Expected fragment '{0}' not found in script at index {1}: {2}",
+                expectedFragment,
+                scriptIndex,
+                sql));
+        }
+    }
+}
