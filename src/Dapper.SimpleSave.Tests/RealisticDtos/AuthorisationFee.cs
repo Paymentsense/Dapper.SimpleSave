c6ec99f419e7adbfb291f2ecb188e511199650b1
Bart Read <bart@bartread.com>
2015-09-11 09:58:45 +0100

    Fixed #40. Added support for user assigned primary keys.

---
+++ src/Dapper.SimpleSave.Tests/RealisticDtos/AuthorisationFee.cs
@@ -0,0 +1,20 @@
+﻿namespace Dapper.SimpleSave.Tests.RealisticDtos
+{
+    [Table("[opp].[AUTHORISATION_FEE_LUT]")]
+    public class AuthorisationFee
+    {
+        [PrimaryKey(false)]
+        public int? FieldItemKey { get; set; }
+
+        public decimal Value { get; set; }
+
+        public decimal Revenue { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public int CalculatorVersionKey { get; set; }
+
+        [SimpleSaveIgnore]
+        public string Name { get; set; }
+    }
+}
